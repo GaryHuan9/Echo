242d0214f68d8312201b0abfba79e64b02a5a32e
MMXXXVIII <henhaodegaigai@gmail.com>
2020-11-04 23:29:56 -0500

    Tiled rendering working. Have some bugs

--- ForceRenderer/Renderers/PixelWorker.cs
+++ ForceRenderer/Renderers/PixelWorker.cs
@@ -3,35 +3,33 @@ using System.Runtime.CompilerServices;
 using System.Threading;
 using CodeHelpers.Vectors;
 using ForceRenderer.Mathematics;
-using ForceRenderer.Scenes;
 
 namespace ForceRenderer.Renderers
 {
 	public abstract class PixelWorker
 	{
-		protected PixelWorker(RenderProfile profile)
+		protected PixelWorker(RenderEngine.Profile profile)
 		{
 			this.profile = profile;
-			scene = profile.scene;
-			pressedScene = profile.pressedScene;
-
 			threadRandom = new ThreadLocal<Random>(() => new Random(Thread.CurrentThread.ManagedThreadId ^ Environment.TickCount));
 		}
 
 		readonly ThreadLocal<Random> threadRandom;
-
-		protected readonly RenderProfile profile;
-		protected readonly Scene scene;
-		protected readonly PressedScene pressedScene;
+		protected readonly RenderEngine.Profile profile;
 
 		protected float RandomValue => (float)threadRandom.Value.NextDouble();
 
+		/// <summary>
+		/// Sample and render at a specific point.
+		/// </summary>
+		/// <param name="uv">The screen percentage point to work on. X should be normalized and between -0.5 to 0.5;
+		/// Y should have the same scale as X and it would depend on the aspect ratio.</param>
 		public abstract Float3 Render(Float2 uv);
 
 		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 		protected bool TryTrace(in Ray ray, out float distance, out int token)
 		{
-			distance = pressedScene.GetIntersection(ray, out token);
+			distance = profile.pressed.GetIntersection(ray, out token);
 			return distance < float.PositiveInfinity;
 		}
 	}
--- ForceRenderer/Renderers/RenderEngine.cs
+++ ForceRenderer/Renderers/RenderEngine.cs
@@ -1,53 +1,30 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using System.Threading;
-using System.Threading.Tasks;
 using CodeHelpers;
 using CodeHelpers.Vectors;
 using ForceRenderer.IO;
-using ForceRenderer.Mathematics;
+using ForceRenderer.Objects;
 using ForceRenderer.Scenes;
 
 namespace ForceRenderer.Renderers
 {
-	public class RenderEngine
+	public class RenderEngine : IDisposable
 	{
-		public RenderEngine(Scene scene, int pixelSample)
-		{
-			this.scene = scene;
-			this.pixelSample = pixelSample;
+		public RenderEngine() => TileWorker.OnWorkCompleted += OnTileWorkCompleted;
 
-			threadRandom = new ThreadLocal<Random>(() => new Random(Thread.CurrentThread.ManagedThreadId));
-			sampleSpiralOffsets = new Float2[pixelSample];
+		public Scene Scene { get; set; }
+		public int PixelSample { get; set; }
 
-			//Create sample spiral offset positions
-			for (int i = 0; i < pixelSample; i++)
-			{
-				float index = i + 0.5f;
-
-				float length = (float)Math.Sqrt(index / pixelSample);
-				float angle = Scalars.PI * (1f + (float)Math.Sqrt(5d)) * index;
-
-				Float2 sample = new Float2((float)Math.Cos(angle), (float)Math.Sin(angle));
-				sampleSpiralOffsets[i] = sample * length / 2f + Float2.half;
-			}
-		}
-
-		public readonly Scene scene;
-		public readonly int pixelSample; //Sample per pixel
+		public int TileSize { get; set; }
+		public int WorkerSize { get; set; } = Environment.ProcessorCount;
 
 		public int MaxBounce { get; set; } = 32;
 		public float EnergyEpsilon { get; set; } = 1E-3f; //Epsilon lower bound value to determine when an energy is essentially zero
 
-		readonly ThreadLocal<Random> threadRandom;
-		readonly Float2[] sampleSpiralOffsets;
-
-		PressedScene pressedScene;
-		Thread renderThread;
-		RenderProfile profile;
+		Profile profile;
 
 		volatile Texture _renderBuffer;
-		long _currentState;
+		volatile int _currentState;
 
 		public Texture RenderBuffer
 		{
@@ -61,161 +38,116 @@ namespace ForceRenderer.Renderers
 
 		public State CurrentState
 		{
-			get => (State)Interlocked.Read(ref _currentState);
-			private set => Interlocked.Exchange(ref _currentState, (long)value);
+			get => (State)Interlocked.CompareExchange(ref _currentState, 0, 0);
+			private set => Interlocked.Exchange(ref _currentState, (int)value);
 		}
 
 		public bool Completed => CurrentState == State.completed;
 
-		TileWorker primaryWorker;
-		TileWorker secondaryWorker;
+		volatile int dispatchedTileCount;
+
+		public int DispatchedTileCount => Interlocked.CompareExchange(ref dispatchedTileCount, 0, 0);
+		public int TotalTileCount => tilePattern?.Length ?? 0; //The number of processed tiles or tiles currently being processed
+
+		TileWorker[] workers;
+		TilePattern tilePattern;
+
+		readonly object manageLocker = new object(); //Locker used when managing any of the workers
 
 		public void Begin()
 		{
 			if (RenderBuffer == null) throw ExceptionHelper.Invalid(nameof(RenderBuffer), this, InvalidType.isNull);
 			if (CurrentState != State.waiting) throw new Exception("Incorrect state! Must reset before rendering!");
 
-			pressedScene = new PressedScene(scene);
-			if (pressedScene.camera == null) throw new Exception("No camera in scene! Cannot render without a camera!");
+			PressedScene pressed = new PressedScene(Scene);
+			profile = new Profile(pressed, this);
 
-			profile = new RenderProfile(scene, pressedScene, pixelSample, RenderBuffer.aspect, MaxBounce, EnergyEpsilon);
-			renderThread = new Thread(RenderThread) {Priority = ThreadPriority.Highest, IsBackground = true};
+			if (pressed.camera == null) throw new Exception("No camera in scene! Cannot render without a camera!");
+			if (PixelSample <= 0) throw ExceptionHelper.Invalid(nameof(PixelSample), PixelSample, "must be positive!");
+			if (TileSize <= 0) throw ExceptionHelper.Invalid(nameof(TileSize), TileSize, "must be positive!");
+			if (MaxBounce < 0) throw ExceptionHelper.Invalid(nameof(MaxBounce), MaxBounce, "cannot be negative!");
+			if (EnergyEpsilon < 0f) throw ExceptionHelper.Invalid(nameof(EnergyEpsilon), EnergyEpsilon, "cannot be negative!");
 
 			CurrentState = State.rendering;
-			renderThread.Start();
+			InitializeWorkers();
 		}
 
-		public void WaitForRender() => renderThread.Join();
-		public void Stop() => CurrentState = State.stopped;
-
-		public void Reset()
+		void InitializeWorkers()
 		{
-			CurrentState = State.waiting;
-			primaryWorker = secondaryWorker = null;
+			tilePattern = new TilePattern(RenderBuffer.size, profile.tileSize);
 
-			profile = default;
-			pressedScene = null;
-			renderThread = null;
-		}
+			SamplePattern samplePattern = new SamplePattern(profile.pixelSample);
+			PixelWorker pixelWorker = new PathTraceWorker(profile);
 
-		void RenderThread()
-		{
-			primaryWorker = new TileWorker(pixelSample,);
-
-			CurrentState = State.completed;
-		}
-
-		Shade RenderIndex(int index)
-		{
-			//Final buffer, need high precision
-			double r = 0d;
-			double g = 0d;
-			double b = 0d;
-
-			for (int i = 1; i <= pixelSample; i++)
+			for (int i = 0; i < profile.workerSize; i++)
 			{
-				Int2 position = profile.buffer.ToPosition(index); //Integer pixel position
-				Float2 offset = sampleSpiralOffsets[i - 1];       //Multi sample offset; from zero to one; generated before render started
-				Float3 single = RenderPixel(profile.buffer.ToUV(position + offset));
+				TileWorker worker = new TileWorker(profile);
 
-				//Combine colors by averaging all samples
-				double multiplier = (i - 1d) / i;
-
-				r = r * multiplier + (double)single.x / i;
-				g = g * multiplier + (double)single.y / i;
-				b = b * multiplier + (double)single.z / i;
+				worker.ResetParameters(Int2.zero, RenderBuffer, pixelWorker, samplePattern);
+				DispatchWorker(worker);
 			}
+		}
 
-			//Instead of doing total / count, this averaging method avoid precision loss
-			return new Shade((float)r, (float)g, (float)b);
+		void DispatchWorker(TileWorker worker)
+		{
+			lock (manageLocker)
+			{
+				int count = DispatchedTileCount;
+				if (count == tilePattern.Length) return;
+
+				worker.ResetParameters(tilePattern[count]);
+				worker.Dispatch();
+
+				Interlocked.Increment(ref dispatchedTileCount);
+			}
+		}
+
+		void OnTileWorkCompleted(TileWorker worker)
+		{
+			lock (manageLocker)
+			{
+				if (DispatchedTileCount == tilePattern.Length) CurrentState = State.completed;
+				else if (CurrentState == State.rendering) DispatchWorker(worker);
+			}
 		}
 
 		/// <summary>
-		/// Renders a single pixel and returns the result.
+		/// Aborts current render session.
 		/// </summary>
-		/// <param name="uv">Zero to one normalized raw uv without any scaling.</param>
-		Float3 RenderPixel(Float2 uv)
+		public void Abort()
 		{
-			Float2 scaled = new Float2(uv.x - 0.5f, (uv.y - 0.5f) / RenderAspect);
-			Ray ray = new Ray(pressedScene.camera.Position, pressedScene.camera.GetDirection(scaled));
+			//TODO Stop the workers
+			CurrentState = State.stopped;
+		}
 
-			int bounce = 0;
-
-			Float3 energy = Float3.one;
-			Float3 color = Float3.zero;
-
-			while (TryTrace(ray, out float distance, out int token) && bounce++ < profile.maxBounce)
+		/// <summary>
+		/// Resets engine for another rendering.
+		/// </summary>
+		public void Reset()
+		{
+			if (workers != null)
 			{
-				ref PressedBundle bundle = ref pressedScene.GetPressedBundle(token);
-
-				Float3 position = ray.GetPoint(distance);
-				Float3 normal = pressedScene.GetNormal(position, token);
-
-				//Lambert diffuse
-				ray = new Ray(position, GetHemisphereDirection(normal), true);
-				energy *= 2f * normal.Dot(ray.direction).Clamp(0f, 1f) * bundle.material.albedo;
-
-				// if (pressedScene.directionalLight != null)
-				// {
-				// 	DirectionalLight light = pressedScene.directionalLight;
-				// 	Ray lightRay = new Ray(position, -light.Direction, true);
-				//
-				// 	float coefficient = normal.Dot(lightRay.direction).Clamp(0f, 1f);
-				// 	if (coefficient > 0f) coefficient *= TryTraceShadow(lightRay);
-				//
-				// 	color += coefficient * energy * bundle.material.albedo * light.Intensity;
-				// }
-
-				// ray = new Ray(position, ray.direction.Reflect(normal), true);
-				// energy *= bundle.material.specular;
-
-				if (energy.x <= profile.energyEpsilon && energy.y <= profile.energyEpsilon && energy.z <= profile.energyEpsilon) break;
+				for (int i = 0; i < workers.Length; i++) workers[i].Dispose();
 			}
 
-			//return (Float3)((float)bounce / profile.maxBounce);
+			workers = null;
+			profile = default;
 
-			if (scene.Cubemap == null) return color;
-			return color + energy * (Float3)scene.Cubemap.Sample(ray.direction) * 1.8f; //Sample skybox
+			tilePattern = null;
+			dispatchedTileCount = 0;
+
+			GC.Collect();
+			CurrentState = State.waiting;
 		}
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-		bool TryTrace(in Ray ray, out float distance, out int token)
+		public void Dispose()
 		{
-			distance = pressedScene.GetIntersection(ray, out token);
-			return distance < float.PositiveInfinity;
-		}
+			if (workers != null)
+			{
+				for (int i = 0; i < workers.Length; i++) workers[i].Dispose();
+			}
 
-		float TryTraceShadow(in Ray ray)
-		{
-			float distance = pressedScene.GetIntersection(ray);
-			return distance < float.PositiveInfinity ? 0f : 1f;
-		}
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
-		Float3 GetHemisphereDirection(Float3 normal)
-		{
-			//Uniformly sample directions in a hemisphere
-			float cos = RandomValue;
-			float sin = (float)Math.Sqrt(Math.Max(0f, 1f - cos * cos));
-			float phi = Scalars.TAU * RandomValue;
-
-			float x = (float)Math.Cos(phi) * sin;
-			float y = (float)Math.Sin(phi) * sin;
-			float z = cos;
-
-			//Transform local direction to world-space based on normal
-			Float3 helper = Math.Abs(normal.x) >= 0.9f ? Float3.forward : Float3.right;
-
-			Float3 tangent = Float3.Cross(normal, helper).Normalized;
-			Float3 binormal = Float3.Cross(normal, tangent).Normalized;
-
-			//Transforms using matrix multiplication. 3x3 matrix instead of 4x4 because direction only
-			return new Float3
-			(
-				x * tangent.x + y * binormal.x + z * normal.x,
-				x * tangent.y + y * binormal.y + z * normal.y,
-				x * tangent.z + y * binormal.z + z * normal.z
-			);
+			TileWorker.OnWorkCompleted -= OnTileWorkCompleted;
 		}
 
 		public enum State
@@ -224,6 +156,38 @@ namespace ForceRenderer.Renderers
 			rendering,
 			completed,
 			stopped
+		}
+
+		/// <summary>
+		/// An immutable structure that is stores a copy of the renderer's settings/profile.
+		/// This ensures that the renderer never changes its settings when all threads are running.
+		/// </summary>
+		public readonly struct Profile
+		{
+			public Profile(PressedScene pressed, RenderEngine engine)
+			{
+				this.pressed = pressed;
+				scene = pressed.source;
+				camera = pressed.camera;
+
+				pixelSample = engine.PixelSample;
+				tileSize = engine.TileSize;
+				workerSize = engine.WorkerSize;
+
+				maxBounce = engine.MaxBounce;
+				energyEpsilon = engine.EnergyEpsilon;
+			}
+
+			public readonly PressedScene pressed;
+			public readonly Scene scene;
+			public readonly Camera camera;
+
+			public readonly int pixelSample;
+			public readonly int tileSize;
+			public readonly int workerSize;
+
+			public readonly int maxBounce;
+			public readonly float energyEpsilon;
 		}
 	}
 }
