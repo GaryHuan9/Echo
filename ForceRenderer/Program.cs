64151a3c63a4dc282aebf099b9660c2e1a4a86aa
MMXXXVIII <henhaodegaigai@gmail.com>
2020-12-25 13:56:42 -0500

    Removed backface culling

--- ForceRenderer/Program.cs
+++ ForceRenderer/Program.cs
@@ -9,6 +9,8 @@ using CodeHelpers.Mathematics;
 using CodeHelpers.Mathematics.Enumerables;
 using CodeHelpers.Threads;
 using ForceRenderer.IO;
+using ForceRenderer.Mathematics;
+using ForceRenderer.Objects.SceneObjects;
 using ForceRenderer.Renderers;
 using ForceRenderer.Terminals;
 
@@ -62,11 +64,11 @@ namespace ForceRenderer
 				new Int2(320, 180), new Int2(854, 480), new Int2(1920, 1080), new Int2(3840, 2160), new Int2(1024, 1024), new Int2(512, 512)
 			};
 
-			Texture buffer = new Texture(resolutions[2]);
+			Texture buffer = new Texture(resolutions[1]);
 			using RenderEngine engine = new RenderEngine
 										{
-											RenderBuffer = buffer, Scene = new LightedBMWScene(),
-											PixelSample = 128, AdaptiveSample = 3200, TileSize = 32
+											RenderBuffer = buffer, Scene = new TestTexture(),
+											PixelSample = 32, AdaptiveSample = 256, TileSize = 32
 										};
 
 			renderEngine = engine;
@@ -83,7 +85,7 @@ namespace ForceRenderer
 			commandsController.Log($"Completed after {elapsedSeconds:F2} seconds with {completedSample:N0} samples at {completedSample / elapsedSeconds:N0} samples per second.");
 
 			renderEngine = null;
-			Console.ReadKey();
+			Thread.Sleep((int)(elapsedSeconds * 1000));
 		}
 
 		static RenderEngine renderEngine;
