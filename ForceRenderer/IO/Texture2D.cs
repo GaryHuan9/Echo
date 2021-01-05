f85126ff0014252d2179ee37b9c12dac13b76ba6
MMXXXVIII <henhaodegaigai@gmail.com>
2021-01-05 17:23:04 -0500

    Rewriting textures

---
+++ ForceRenderer/IO/Texture2D.cs
@@ -0,0 +1,11 @@
+namespace ForceRenderer.IO
+{
+	/// <summary>
+	/// The default texture; stores RGBA color information with 8 bits per channel.
+	/// Supports saving and loading from image files.
+	/// </summary>
+	public class Texture2D : Texture
+	{
+
+	}
+}
