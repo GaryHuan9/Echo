f5d37d9a3247a776434afaa02b711ba24235fa07
MMXXXVIII <henhaodegaigai@gmail.com>
2021-01-29 17:49:00 -0500

    Optimized BVH to use 64 byte cache line

---
+++ IntrinsicsSIMD/BenchmarkBVH.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using CodeHelpers.Mathematics;
+using ForceRenderer;
+using ForceRenderer.IO;
+using ForceRenderer.Mathematics;
+using ForceRenderer.Objects.SceneObjects;
+using ForceRenderer.Renderers;
+
+namespace IntrinsicsSIMD
+{
+	public class BenchmarkBVH
+	{
+		public BenchmarkBVH()
+		{
+			Scene scene = new Scene();
+			Random random = new Random(42);
+
+			scene.children.Add(new MeshObject(new Mesh(@"C:\Users\MMXXXVIII\Things\CodingStuff\C#\ForceRenderer\ForceRenderer\Assets\Models\BlenderBMW\BlenderBMW.obj")));
+
+			bvh = new PressedScene(scene).bvh;
+			rays = new Ray[65536];
+
+			const float Radius = 6f;
+			const float Height = 12f;
+
+			for (int i = 0; i < rays.Length; i++)
+			{
+				var position = new Float3(Random() * Radius, Random() * Height, 0f).RotateXZ(Random() * 360f);
+				rays[i] = new Ray(position, (new Float3(0f, 1.5f, 0f) - position).Normalized);
+			}
+
+			float Random() => (float)random.NextDouble();
+		}
+
+		readonly BoundingVolumeHierarchy bvh;
+		readonly Ray[] rays;
+
+		//V0: 69us per 128 intersections = 540ns per intersection
+		//V1:
+
+		[Benchmark]
+		public void GetIntersection()
+		{
+			for (int i = 0; i < rays.Length; i++) bvh.GetIntersection(rays[i]);
+		}
+	}
+
+}
