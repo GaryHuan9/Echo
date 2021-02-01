03237e37d240ab9fd2aff5075b11712550556667
MMXXXVIII <henhaodegaigai@gmail.com>
2021-02-01 18:55:02 -0500

    Rewriting materials

---
+++ ForceRenderer/IO/MaterialNew.cs
@@ -0,0 +1,10 @@
+﻿using CodeHelpers.Mathematics;
+using ForceRenderer.Mathematics;
+
+namespace ForceRenderer.IO
+{
+	public abstract class MaterialNew
+	{
+		public abstract Float3 BidirectionalDistribution(in Ray ray, in Hit hit, in Float3 normal, out Float3 direction);
+	}
+}
---
+++ ForceRenderer/Renderers/PathTraceWorkerNew.cs
@@ -0,0 +1,47 @@
+﻿using CodeHelpers.Mathematics;
+using ForceRenderer.Mathematics;
+
+namespace ForceRenderer.Renderers
+{
+	public class PathTraceWorkerNew : PixelWorker
+	{
+		public PathTraceWorkerNew(RenderEngine.Profile profile) : base(profile) { }
+
+		/// <summary>
+		/// Returns a random vector inside a unit sphere.
+		/// </summary>
+		Float3 RandomInSphere
+		{
+			get
+			{
+				Float3 random;
+
+				do random = new Float3(RandomValue * 2f - 1f, RandomValue * 2f - 1f, RandomValue * 2f - 1f);
+				while (random.SquaredMagnitude > 1f);
+
+				return random;
+			}
+		}
+
+		/// <summary>
+		/// Returns a random unit vector that is on a unit sphere.
+		/// </summary>
+		Float3 RandomOnSphere => RandomInSphere.Normalized;
+
+		public override Float3 Render(Float2 screenUV)
+		{
+			Ray ray = new Ray(profile.camera.Position, profile.camera.GetDirection(screenUV));
+
+			for (int bounce = 0; bounce < profile.maxBounce; bounce++)
+			{
+				if (!GetIntersection(ray, out Hit hit)) break;
+
+				Float3 position = ray.GetPoint(hit.distance);
+				Float3 normal = profile.pressed.GetNormal(hit);
+
+				Float3 direction;
+				Float3 bsdf; //Bidirectional scattering distribution function
+			}
+		}
+	}
+}
