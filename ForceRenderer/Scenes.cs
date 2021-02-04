d95f93c0e8348f302640393d5a88cb4f197777a7
MMXXX-VIII <henhaodegaigai@gmail.com>
2021-02-04 17:27:58 -0500

    Glass roughness?

--- ForceRenderer/Scenes.cs
+++ ForceRenderer/Scenes.cs
@@ -255,7 +255,8 @@ namespace ForceRenderer
 			children.Add(new Camera(90f) {Position = new Float3(0f, 1f, -2f)});
 
 			children.Add(new SphereObject(new Diffuse {Albedo = (Float3)0.9f}, 1f) {Position = new Float3(0f, 1f, 2f)});
-			children.Add(new SphereObject(new Metal {Albedo = (Float3)0.7f, Smoothness = 0.6f}, 1f) {Position = new Float3(2f, 1f, 2f)});
+			// children.Add(new SphereObject(new Metal {Albedo = (Float3)0.7f, Smoothness = 0.5f}, 1f) {Position = new Float3(2f, 1f, 2f)});
+			children.Add(new SphereObject(new Glass {Transmission = (Float3)0.9f, IndexOfRefraction = 1.5f, Roughness = 0.5f}, 1f) {Position = new Float3(2f, 1f, 2f)});
 			children.Add(new SphereObject(new Glass {Transmission = (Float3)0.9f, IndexOfRefraction = 1.5f}, 1f) {Position = new Float3(-2f, 1f, 2f)});
 
 			children.Add(new PlaneObject(new Diffuse {Albedo = (Float3)0.9f}, new Float2(24f, 16f)) {Position = new Float3(0f, 0f, 0f)});
