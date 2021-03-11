e7e00dd3efd6c5b04e5cec0a0ce0f31f0e3b85fa
MMXXXVIII <henhaodegaigai@gmail.com>
2021-03-10 19:16:18 -0500

    Revert "Reverting texture changes"
    This reverts commit ca955319b1d88f139121e3f7599bd07586d48ccd.

--- ForceRenderer/Rendering/PostProcessing/Denoiser.cs
+++ ForceRenderer/Rendering/PostProcessing/Denoiser.cs
@@ -30,8 +30,8 @@ namespace ForceRenderer.Rendering.PostProcessing
 
 			foreach (Int2 local in new EnumerableSpace2D((Int2)(-radius), (Int2)radius))
 			{
-				averageSource += sourceBuffer.GetPixel(position + local).XYZ;
-				averageAlbedo += albedoBuffer.GetPixel(position + local).XYZ;
+				averageSource += sourceBuffer[position + local].XYZ;
+				averageAlbedo += albedoBuffer[position + local].XYZ;
 
 				count++;
 			}
@@ -39,12 +39,14 @@ namespace ForceRenderer.Rendering.PostProcessing
 			averageSource /= count;
 			averageAlbedo /= count;
 
-			Float3 differenceSource = (averageSource - sourceBuffer.GetPixel(position).XYZ).Absoluted / averageSource;
-			Float3 differenceAlbedo = (averageAlbedo - albedoBuffer.GetPixel(position).XYZ).Absoluted / averageAlbedo;
+			Float3 differenceSource = (averageSource - sourceBuffer[position].XYZ).Absoluted / averageSource;
+			Float3 differenceAlbedo = (averageAlbedo - albedoBuffer[position].XYZ).Absoluted / averageAlbedo;
 
 			float luminance = GetLuminance(differenceSource) - GetLuminance(differenceAlbedo);
-			renderBuffer.SetPixel(position, (Float3)Math.Abs(luminance));
+			renderBuffer[position] = ToColor((Float3)Math.Abs(luminance));
 		}
+
+		static Float4 ToColor(in Float3 color) => new Float4(color.x, color.y, color.z, 1f);
 
 		static float GetSaturation(in Float3 color)
 		{
