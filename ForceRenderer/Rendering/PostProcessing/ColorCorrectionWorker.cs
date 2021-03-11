c85ce071b070e5cc82973bb30712d710aae1e07f
MMXXXVIII <henhaodegaigai@gmail.com>
2021-03-10 19:15:34 -0500

    Reverting texture changes

--- ForceRenderer/Rendering/PostProcessing/ColorCorrectionWorker.cs
+++ ForceRenderer/Rendering/PostProcessing/ColorCorrectionWorker.cs
@@ -21,18 +21,18 @@ namespace ForceRenderer.Rendering.PostProcessing
 
 		unsafe void GammaCorrectPass(Int2 position) //https://www.desmos.com/calculator/v9a3uscr8c
 		{
-			ref Vector128<float> target = ref renderBuffer.GetPixel(position);
+			Vector128<float> pixel = renderBuffer[position];
 
-			Vector128<float> a = Sse.Divide(Sse.Subtract(target, oneVector), smoothnessVector);
+			Vector128<float> a = Sse.Divide(Sse.Subtract(pixel, oneVector), smoothnessVector);
 			Vector128<float> h = Sse.Subtract(halfVector, Sse.Multiply(halfVector, a));
 
 			h = Sse.Min(Sse.Max(h, zeroVector), oneVector);
 
-			Vector128<float> b = Sse.Subtract(Sse.Subtract(target, oneVector), smoothnessVector);
+			Vector128<float> b = Sse.Subtract(Sse.Subtract(pixel, oneVector), smoothnessVector);
 			Vector128<float> result = Sse.Add(Sse.Multiply(Sse.Add(b, Sse.Multiply(smoothnessVector, h)), h), oneVector);
 
 			*((float*)&result + 3) = 1f; //Set alpha to one
-			target = result;
+			renderBuffer[position] = result;
 		}
 	}
 }
