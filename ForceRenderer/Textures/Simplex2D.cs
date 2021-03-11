8aacfb8d3b5ea8fb5167c3d6fa26ba13eb85a6f4
MMXXXVIII <henhaodegaigai@gmail.com>
2021-03-11 18:00:25 -0500

    Fractional not fractal!!

--- ForceRenderer/Textures/Simplex2D.cs
+++ ForceRenderer/Textures/Simplex2D.cs
@@ -32,20 +32,20 @@ namespace ForceRenderer.Textures
 
 		protected override Vector128<float> Sample(Float2 position)
 		{
-			// Float2 q = new Float2(FractalBrownianMotion(position), FractalBrownianMotion(position + new Float2(5.2f, 1.3f)));
-			// Float2 r = new Float2(FractalBrownianMotion(position + 4f * q + new Float2(1.7f, 9.2f)), FractalBrownianMotion(position + 4f * q + new Float2(8.3f, 2.8f)));
+			// Float2 q = new Float2(FractionalBrownianMotion(position), FractionalBrownianMotion(position + new Float2(5.2f, 1.3f)));
+			// Float2 r = new Float2(FractionalBrownianMotion(position + 4f * q + new Float2(1.7f, 9.2f)), FractionalBrownianMotion(position + 4f * q + new Float2(8.3f, 2.8f)));
 			//
-			// float v = FractalBrownianMotion(position + 4f * r);
+			// float v = FractionalBrownianMotion(position + 4f * r);
 
-			float v0 = FractalBrownianMotion(position);
+			float v0 = FractionalBrownianMotion(position);
 
-			float v1 = FractalBrownianMotion(position + new Float2(v0 * 5.2f, v0 * 2.3f));
-			float v2 = FractalBrownianMotion(position + new Float2(v1 * 2.7f, v1 * 4.7f));
+			float v1 = FractionalBrownianMotion(position + new Float2(v0 * 5.2f, v0 * 2.3f));
+			float v2 = FractionalBrownianMotion(position + new Float2(v1 * 2.7f, v1 * 4.7f));
 
 			return Utilities.ToVector(Utilities.ToColor(new Float3(v1, v2, 1f)));
 		}
 
-		float FractalBrownianMotion(Float2 position)
+		float FractionalBrownianMotion(Float2 position)
 		{
 			float sum = 0f;
 			float frequency = 1f;
