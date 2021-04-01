fefdff8ae4d1fa1492db0448fac0ae7137d90f7a
MMXXX-VIII <henhaodegaigai@gmail.com>
2021-04-01 15:01:18 -0400

    Adding mesh instances

--- ForceRenderer/Mathematics/Intersections/PressedPackInstance.cs
+++ ForceRenderer/Mathematics/Intersections/PressedPackInstance.cs
@@ -2,17 +2,26 @@
 
 namespace ForceRenderer.Mathematics.Intersections
 {
-	public class GeometryPackInstance
+	public class PressedPackInstance
 	{
-		readonly GeometryPack pack;
+		readonly PressedPack pack;
 
-		readonly Float4x4 forwardTransform;
-		readonly Float4x4 backwardTransform;
+		readonly Float4x4 forwardTransform; //This transform only transforms from the parent space to local space
+
+		readonly Float4x4 backwardTransform; //These two properties are transforms from local space to world space directly,
+		readonly float backwardScaling;      //since our backward transform does not recurse up the instance tree
 
 		public void GetIntersection(in Ray ray, ref Hit hit)
 		{
+			float distance = hit.distance;
 
+			pack.bvh.GetIntersection(TransformForward(ray), ref hit);
+			if (Scalars.AlmostEquals(distance, hit.distance)) return;
+
+			hit.instance ??= this;
 		}
+
+		public int GetIntersectionCost(in Ray ray, ref float distance) => pack.bvh.GetIntersectionCost(TransformForward(ray), ref distance);
 
 		public Ray TransformForward(in Ray ray)
 		{
