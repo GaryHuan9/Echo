db0c73738dca9c7dc44062d8ef77858527074690
MMXXXVIII <henhaodegaigai@gmail.com>
2021-04-18 12:24:11 -0400

    Tried Edge Avoiding A Trous Wavelet Transform

--- EchoRenderer/Rendering/PostProcessing/Denoiser.cs
+++ EchoRenderer/Rendering/PostProcessing/Denoiser.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
 using CodeHelpers.Mathematics;
 using CodeHelpers.Mathematics.Enumerables;
 using EchoRenderer.Textures;
@@ -7,55 +9,129 @@ namespace EchoRenderer.Rendering.PostProcessing
 {
 	public class Denoiser : PostProcessingWorker
 	{
-		public Denoiser(PostProcessingEngine engine, Texture albedoBuffer) : base(engine) => this.albedoBuffer = albedoBuffer;
+		public Denoiser(PostProcessingEngine engine, Texture normalBuffer, Texture positionBuffer) : base(engine)
+		{
+			this.normalBuffer = normalBuffer;
+			this.positionBuffer = positionBuffer;
+		}
 
-		Texture sourceBuffer;
-		Texture albedoBuffer;
+		public float ColorCoefficient { get; set; } = 0.01f;
+		public float NormalCoefficient { get; set; } = 0.001f;
+		public float PositionCoefficient { get; set; } = 0.001f;
+
+		//Denoiser based on: https://jo.dreggn.org/home/2010_atrous.pdf
+
+		readonly Texture normalBuffer;
+		readonly Texture positionBuffer;
+
+		readonly float[] kernels =
+		{
+			1f / 256f, 1f / 64f, 3f / 128f, 1f / 64f, 1f / 256f,
+			1f / 64f, 1f / 16f, 3f / 32f, 1f / 16f, 1f / 64f,
+			3f / 128f, 3f / 32f, 9f / 64f, 3f / 32f, 3f / 128f,
+			1f / 64f, 1f / 16f, 3f / 32f, 1f / 16f, 1f / 64f,
+			1f / 256f, 1f / 64f, 3f / 128f, 1f / 64f, 1f / 256f
+		};
+
+		readonly Int2[] offsets =
+		{
+			new(-2, 2), new(-1, 2), new(0, 2), new(1, 2), new(2, 2),
+			new(-2, 1), new(-1, 1), new(0, 1), new(1, 1), new(2, 1),
+			new(-2, 0), new(-1, 0), new(0, 0), new(1, 0), new(2, 0),
+			new(-2, -1), new(-1, -1), new(0, -1), new(1, -1), new(2, -1),
+			new(-2, -2), new(-1, -2), new(0, -2), new(1, -2), new(2, -2)
+		};
+
+		int kernelRadius;
+		Vector128<float> sigmas;
+
+		static readonly Vector128<float> zeroVector = Vector128.Create(0f);
+		static readonly Vector128<float> oneVector = Vector128.Create(1f);
 
 		public override void Dispatch()
 		{
-			sourceBuffer = new Texture2D(renderBuffer.size);
+			sigmas = Vector128.Create
+			(
+				ColorCoefficient.Clamp(Scalars.Epsilon, 1f),
+				NormalCoefficient.Clamp(Scalars.Epsilon, 1f),
+				PositionCoefficient.Clamp(Scalars.Epsilon, 1f),
+				1f
+			);
 
-			RunCopyPass(renderBuffer, sourceBuffer);
-			RunPass(SaturationPass);
+			for (int i = 0; i < 7; i++)
+			{
+				kernelRadius = 1 << i;
+				RunPass(SinglePass);
+
+				// SinglePass(new Int2(300, 300));
+			}
 		}
 
-		void SaturationPass(Int2 position)
+		void SinglePass(Int2 position)
 		{
-			Float3 averageSource = Float3.zero;
-			Float3 averageAlbedo = Float3.zero;
+			var sum = zeroVector;
+			float total = 0f;
 
-			float count = 0f;
-			int radius = 3;
+			ref Vector128<float> targetColor = ref renderBuffer.GetPixel(position);
 
-			foreach (Int2 local in new EnumerableSpace2D((Int2)(-radius), (Int2)radius))
+			ref readonly Vector128<float> targetNormal = ref normalBuffer.GetPixel(position);
+			ref readonly Vector128<float> targetPosition = ref positionBuffer.GetPixel(position);
+
+			for (int i = 0; i < kernels.Length; i++)
 			{
-				averageSource += sourceBuffer[position + local].XYZ;
-				averageAlbedo += albedoBuffer[position + local].XYZ;
+				Int2 local = position + offsets[i] * kernelRadius;
+				if (renderBuffer.Restrict(local) != local) continue;
 
-				count++;
+				ref readonly Vector128<float> localColor = ref renderBuffer.GetPixel(local);
+				ref readonly Vector128<float> localNormal = ref normalBuffer.GetPixel(local);
+				ref readonly Vector128<float> localPosition = ref positionBuffer.GetPixel(local);
+
+				Vector128<float> deltaColor = Sse.Subtract(targetColor, localColor);
+				Vector128<float> deltaNormal = Sse.Subtract(targetNormal, localNormal);
+				Vector128<float> deltaPosition = Sse.Subtract(targetPosition, localPosition);
+
+				deltaColor = Sse41.DotProduct(deltaColor, deltaColor, 0b1110_0001);
+				deltaNormal = Sse41.DotProduct(deltaNormal, deltaNormal, 0b1110_0010);
+				deltaPosition = Sse41.DotProduct(deltaPosition, deltaPosition, 0b1110_0100);
+
+				unsafe
+				{
+					float* pointer = (float*)&deltaNormal + 1;
+					*pointer /= kernelRadius * kernelRadius;
+				}
+
+				Vector128<float> weights = Sse.Add(deltaColor, Sse.Add(deltaNormal, deltaPosition));
+				weights = Sse.Min(Exp(Sse.Divide(Sse.Subtract(zeroVector, weights), sigmas)), oneVector);
+
+				float weight = kernels[i];
+
+				unsafe
+				{
+					float* pointer = (float*)&weights;
+					weight *= pointer[0] * pointer[1] * pointer[2];
+				}
+
+				Vector128<float> weightVector = Vector128.Create(weight);
+				sum = Sse.Add(sum, Sse.Multiply(weightVector, localColor));
+
+				total += weight;
 			}
 
-			averageSource /= count;
-			averageAlbedo /= count;
-
-			Float3 differenceSource = (averageSource - sourceBuffer[position].XYZ).Absoluted / averageSource;
-			Float3 differenceAlbedo = (averageAlbedo - albedoBuffer[position].XYZ).Absoluted / averageAlbedo;
-
-			float luminance = GetLuminance(differenceSource) - GetLuminance(differenceAlbedo);
-			renderBuffer[position] = ToColor((Float3)Math.Abs(luminance));
+			targetColor = Sse.Divide(sum, Vector128.Create(total));
 		}
 
-		static Float4 ToColor(in Float3 color) => new Float4(color.x, color.y, color.z, 1f);
-
-		static float GetSaturation(in Float3 color)
+		/// <summary>
+		/// Calculates and returns the natural exponent of the first three numbers in <paramref name="value"/>.
+		/// </summary>
+		unsafe Vector128<float> Exp(Vector128<float> value)
 		{
-			float min = color.MinComponent;
-			float max = color.MaxComponent;
+			float* pointer = (float*)&value;
 
-			return Scalars.AlmostEquals(max, 0f) ? 0f : (max - min) / max;
+			pointer[0] = MathF.Exp(pointer[0]);
+			pointer[1] = MathF.Exp(pointer[1]);
+			pointer[2] = MathF.Exp(pointer[2]);
+
+			return value;
 		}
-
-		static float GetLuminance(in Float3 color) => color.Dot(new Float3(0.2126f, 0.7152f, 0.0722f));
 	}
 }
---
+++ EchoRenderer/Textures/Scoped.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EchoRenderer.Textures
+{
+	public readonly struct ScopedWrapper : IDisposable
+	{
+		public ScopedWrapper(Texture texture, IWrapper wrapper)
+		{
+			this.texture = texture;
+
+			original = texture.Wrapper;
+			texture.Wrapper = wrapper;
+		}
+
+		readonly Texture texture;
+		readonly IWrapper original;
+
+		public void Dispose() => texture.Wrapper = original;
+	}
+
+	public readonly struct ScopedFilter : IDisposable
+	{
+		public ScopedFilter(Texture texture, IFilter filter)
+		{
+			this.texture = texture;
+
+			original = texture.Filter;
+			texture.Filter = filter;
+		}
+
+		readonly Texture texture;
+		readonly IFilter original;
+
+		public void Dispose() => texture.Filter = original;
+	}
+}
