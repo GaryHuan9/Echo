29e059a2c263f921f2302f163d9a9c9846eb9730
MMXXXVIII <henhaodegaigai@gmail.com>
2021-04-24 00:49:28 -0400

    New night scene requires too many samples
    Former-commit-id: 63c89b7f37eeb8d7e192adf785e87a28c5b68634

--- EchoRenderer/Objects/Scenes/ExcludedScenes.cs
+++ EchoRenderer/Objects/Scenes/ExcludedScenes.cs
@@ -9,14 +9,14 @@ namespace EchoRenderer.Objects.Scenes
 	{
 		public BallRoom()
 		{
-			Cubemap = new SixSideCubemap("Assets/Cubemaps/OutsideDayTime", (Float3)0.15f);
+			Cubemap = new SixSideCubemap("Assets/Cubemaps/OutsideDayTime", (Float3)0.02f);
 
 			var mesh = new Mesh("Assets/Models/Excluded/BallRoom/ballRoom.obj");
 			var materials = new MaterialLibrary("Assets/Models/Excluded/BallRoom/ballRoom.mat");
 
 			children.Add(new MeshObject(mesh, materials));
 
-			children.Add(new Camera(90f) {Position = new Float3(2.2f, 1.7f, -6.9f), Rotation = new Float3(-2f, 76f, 0f)});
+			children.Add(new Camera(84f) {Position = new Float3(2.2f, 1.7f, -7.3f), Rotation = new Float3(-2f, 76f, 0f)});
 		}
 	}
 }
