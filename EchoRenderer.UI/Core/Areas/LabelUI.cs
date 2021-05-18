d65ddaf974b174b5ea42cf05a9b70761c2ea7c26
MMXXXVIII <henhaodegaigai@gmail.com>
2021-05-18 11:22:03 -0400

    Changed something
    Former-commit-id: 278ce902d2092636d15298dd3ffb6befc99d2411

---
+++ EchoRenderer.UI/Core/Areas/LabelUI.cs
@@ -0,0 +1,29 @@
+﻿using CodeHelpers.Diagnostics;
+using CodeHelpers.Mathematics;
+using SFML.Graphics;
+
+namespace EchoRenderer.UI.Core.Areas
+{
+	public class LabelUI : AreaUI
+	{
+		readonly Text display = new Text("Hello World", mono);
+
+		static readonly Font mono = new Font("Assets/Fonts/JetBrainsMono/JetBrainsMono-Bold.ttf");
+
+		protected override void Reorient(Float2 position, Float2 size)
+		{
+			base.Reorient(position, size);
+
+			display.CharacterSize = (uint)size.y;
+			display.Position = position.As();
+		}
+
+		protected override void Paint(RenderTarget renderTarget)
+		{
+			base.Paint(renderTarget);
+			renderTarget.Draw(display);
+
+			DebugHelper.Log(display.GetLocalBounds());
+		}
+	}
+}
