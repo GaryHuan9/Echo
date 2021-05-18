d2c9bc24a4c30a373a96f42da565d1b7857087d6
MMXXXVIII <henhaodegaigai@gmail.com>
2021-05-17 22:13:12 -0400

    Adding UI system

---
+++ EchoRenderer.UI/Core/ConvertUtilities.cs
@@ -0,0 +1,16 @@
+﻿using CodeHelpers.Mathematics;
+using SFML.System;
+
+namespace EchoRenderer.UI.Core
+{
+	public static class ConvertUtilities
+	{
+		public static Vector2f As(this Float2 value) => new Vector2f(value.x, value.y);
+		public static Vector2i As(this Int2 value) => new Vector2i(value.x, value.y);
+		public static Vector2u Cast(this Int2 value) => new Vector2u((uint)value.x, (uint)value.y);
+
+		public static Float2 As(this Vector2f value) => new Float2(value.X, value.Y);
+		public static Int2 As(this Vector2i value) => new Int2(value.X, value.Y);
+		public static Int2 Cast(this Vector2u value) => new Int2((int)value.X, (int)value.Y);
+	}
+}
---
+++ EchoRenderer.UI/Interface/RenderPreviewUI.cs
@@ -0,0 +1,68 @@
+﻿using CodeHelpers.Mathematics;
+using EchoRenderer.Textures;
+using EchoRenderer.UI.Core;
+using EchoRenderer.UI.Core.Areas;
+using Texture = SFML.Graphics.Texture;
+
+namespace EchoRenderer.UI.Interface
+{
+	public class RenderPreviewUI : AreaUI
+	{
+		public RenderPreviewUI()
+		{
+			imageUI = new ImageUI();
+			Add(imageUI);
+		}
+
+		RenderBuffer _renderBuffer;
+
+		public RenderBuffer RenderBuffer
+		{
+			get => _renderBuffer;
+			set
+			{
+				if (_renderBuffer == value) return;
+				Texture texture = imageUI.Texture;
+
+				if (texture != null && texture.Size.Cast() != value.size)
+				{
+					texture.Dispose();
+					texture = null;
+				}
+
+				if (texture == null)
+				{
+					uint width = (uint)value.size.x;
+					uint height = (uint)value.size.y;
+
+					imageUI.Texture = new Texture(width, height);
+					bytesArray = new byte[width * height * 9];
+				}
+
+				_renderBuffer = value;
+			}
+		}
+
+		readonly ImageUI imageUI;
+		byte[] bytesArray;
+
+		public override void Update()
+		{
+			base.Update();
+			if (RenderBuffer == null) return;
+
+			foreach (Int2 position in RenderBuffer.size.Loop())
+			{
+				Color32 pixel = (Color32)RenderBuffer[position];
+				int index = RenderBuffer.ToIndex(position) * 4;
+
+				bytesArray[index + 0] = pixel.r;
+				bytesArray[index + 1] = pixel.g;
+				bytesArray[index + 2] = pixel.b;
+				bytesArray[index + 3] = byte.MaxValue;
+			}
+
+			imageUI.Texture.Update(bytesArray);
+		}
+	}
+}
