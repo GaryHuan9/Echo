a8271d34212e30993136b5d90672bc33b0b117d4
MMXXXVIII <henhaodegaigai@gmail.com>
2021-05-17 22:13:12 -0400

    Adding UI system

---
+++ EchoRenderer.UI/Core/Areas/ImageUI.cs
@@ -0,0 +1,45 @@
+﻿using CodeHelpers.Mathematics;
+using SFML.Graphics;
+
+namespace EchoRenderer.UI.Core.Areas
+{
+	public class ImageUI : AreaUI
+	{
+		public bool KeepAspect { get; set; } = true;
+		public Texture Texture { get; set; }
+
+		readonly RectangleShape display = new RectangleShape();
+
+		protected override void Reorient(Float2 position, Float2 size)
+		{
+			base.Reorient(position, size);
+			if (Texture == null) return;
+
+			if (KeepAspect)
+			{
+				float aspect = (float)Texture.Size.X / Texture.Size.Y;
+				int majorAxis = aspect * size.y / size.x > 1f ? 0 : 1;
+
+				Float2 center = position + size / 2f;
+				float border = size[majorAxis];
+
+				size = new Float2(border * aspect, border);
+				if (majorAxis == 0) size /= aspect;
+
+				position = center - size / 2f;
+			}
+
+			display.Position = position.As();
+			display.Size = size.As();
+		}
+
+		protected override void Paint(RenderTarget renderTarget)
+		{
+			base.Paint(renderTarget);
+			if (Texture == null) return;
+
+			display.Texture = Texture;
+			renderTarget.Draw(display);
+		}
+	}
+}
---
+++ EchoRenderer.UI/Core/Areas/RootUI.cs
@@ -0,0 +1,32 @@
+﻿using CodeHelpers.Mathematics;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace EchoRenderer.UI.Core.Areas
+{
+	public class RootUI : AreaUI
+	{
+		public RootUI(Application application)
+		{
+			this.application = application;
+
+			application.Resized += OnResize;
+		}
+
+		readonly Application application;
+
+		public void Resize(Float2 size)
+		{
+			Reorient(Float2.zero, size);
+			transform.MarkDirty();
+		}
+
+		void OnResize(object sender, SizeEventArgs argument)
+		{
+			Float2 size = new Float2(argument.Width, argument.Height);
+			application.SetView(new View(size.As() / 2f, size.As()));
+
+			Resize(size);
+		}
+	}
+}
