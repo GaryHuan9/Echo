05826acceb83a190e61a818c8bdbc7d0ca178f94
Gary Huang <henhaodegaigai@gmail.com>
2021-10-10 19:09:29 -0400

    Handled AABB edge cases
    Former-commit-id: efe61c432bb19a42a7d4f940cee86c7b854f85e0

--- EchoRenderer/Objects/Scenes/MaterialBalls.cs
+++ EchoRenderer/Objects/Scenes/MaterialBalls.cs
@@ -12,14 +12,14 @@ namespace EchoRenderer.Objects.Scenes
 {
 	public class SingleMaterialBall : StandardScene
 	{
-		public SingleMaterialBall() : base(new Glossy {Albedo = new Float3(0.78f, 0.76f, 0.79f), Smoothness = 0.74f})
+		public SingleMaterialBall() : base(new Glossy { Albedo = new Float3(0.78f, 0.76f, 0.79f), Smoothness = 0.74f })
 		{
 			Cubemap = new SixSideCubemap("Assets/Cubemaps/OutsideDayTime", (Float3)0.85f);
 
 			var mesh = new Mesh("Assets/Models/BlenderMaterialBall/MaterialBall.zip");
 			var materials = new MaterialLibrary("Assets/Models/BlenderMaterialBall/MaterialBall.mat");
 
-			children.Add(new MeshObject(mesh, materials) {Position = new Float3(0f, 0f, -2.5f), Rotation = Float3.up * -75f, Scale = Float3.one * 2f});
+			children.Add(new MeshObject(mesh, materials) { Position = new Float3(0f, 0f, -2.5f), Rotation = Float3.up * -75f, Scale = Float3.one * 2f });
 		}
 	}
 
@@ -38,7 +38,7 @@ namespace EchoRenderer.Objects.Scenes
 			ObjectPack ball = new ObjectPack();
 			Int3 gridSize = new Int3(10, 4, 7);
 
-			ball.children.Add(new MeshObject(mesh, materials) {Rotation = Float3.up * -75f, Scale = (Float3)0.45f});
+			ball.children.Add(new MeshObject(mesh, materials) { Rotation = Float3.up * -75f, Scale = (Float3)0.45f });
 
 			Diffuse coreSource = (Diffuse)materials["Core"];
 			Glass solidSource = (Glass)materials["Solid"];
@@ -50,16 +50,16 @@ namespace EchoRenderer.Objects.Scenes
 			{
 				Float3 percent = Float3.InverseLerp(-gridSize, gridSize, position);
 
-				Material core = cores.TryGetValue(position.YZ) ?? (cores[position.YZ] = new Diffuse {Albedo = Float3.Lerp(Float3.one, Float3.Lerp(coreSource.Albedo, Float3.zero, percent.z), percent.y)});
-				Material solid = solids.TryGetValue(position.x) ?? (solids[position.x] = new Glass {Albedo = solidSource.Albedo, IndexOfRefraction = solidSource.IndexOfRefraction, Roughness = percent.x});
+				Material core = cores.TryGetValue(position.YZ) ?? (cores[position.YZ] = new Diffuse { Albedo = Float3.Lerp(Float3.one, Float3.Lerp(coreSource.Albedo, Float3.zero, percent.z), percent.y) });
+				Material solid = solids.TryGetValue(position.x) ?? (solids[position.x] = new Glass { Albedo = solidSource.Albedo, IndexOfRefraction = solidSource.IndexOfRefraction, Roughness = percent.x });
 
-				MaterialMapper mapper = new MaterialMapper {[coreSource] = core, [solidSource] = solid};
-				children.Add(new ObjectPackInstance(ball) {Position = position.XYZ, Mapper = mapper});
+				MaterialMapper mapper = new MaterialMapper { [coreSource] = core, [solidSource] = solid };
+				children.Add(new ObjectPackInstance(ball) { Position = position.XYZ, Mapper = mapper });
 			}
 
-			children.Add(new Light {Intensity = Utilities.ToColor("#c9e2ff").XYZ, Rotation = new Float3(60f, 60f, 0f)});
+			children.Add(new Light { Intensity = Utilities.ToColor("#c9e2ff").XYZ, Rotation = new Float3(60f, 60f, 0f) });
 
-			Camera camera = new Camera(100f) {Position = new Float3(-5f, 6f, -10f)};
+			Camera camera = new Camera(100f) { Position = new Float3(-5f, 6f, -10f) };
 
 			camera.LookAt(Float3.zero);
 			children.Add(camera);
