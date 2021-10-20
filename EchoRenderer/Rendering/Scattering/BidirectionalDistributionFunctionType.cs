6d323de4784186baa697ce044bcffa623b431de1
Gary Huang <henhaodegaigai@gmail.com>
2021-10-20 00:36:52 -0400

    Rewriting materials

---
+++ EchoRenderer/Rendering/Scattering/BidirectionalDistributionFunctionType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EchoRenderer.Rendering.Scattering
+{
+	[Flags]
+	public enum BidirectionalDistributionFunctionType
+	{
+		reflection   = 1 << 0,
+		transmission = 1 << 1,
+		diffuse      = 1 << 2,
+		glossy       = 1 << 3,
+		specular     = 1 << 4,
+		all          = diffuse | glossy | specular | reflection | transmission
+	}
+}
---
+++ EchoRenderer/Rendering/Scattering/OrenNayar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CodeHelpers.Mathematics;
+
+namespace EchoRenderer.Rendering.Scattering
+{
+	public class OrenNayar : BidirectionalDistributionFunction
+	{
+		public OrenNayar() : base
+		(
+			BidirectionalDistributionFunctionType.reflection |
+			BidirectionalDistributionFunctionType.diffuse
+		) { }
+
+		public void Reset(in Float3 newReflectance, float newSigma)
+		{
+			reflectance = newReflectance;
+			sigma       = newSigma * Scalars.DegreeToRadian;
+
+			float sigma2 = sigma * sigma;
+
+			a = 1f - sigma2 / 2f / (sigma2 + 0.33f);
+			b = 0.45f * sigma2 / (sigma2 + 0.09f);
+		}
+
+		Float3 reflectance;
+		float  sigma;
+
+		float a;
+		float b;
+
+		public override Float3 Sample(in Float3 outgoing, in Float3 incident)
+		{
+			float sinO = Sine(outgoing);
+			float sinI = Sine(incident);
+
+			throw new NotImplementedException();
+		}
+	}
+}
