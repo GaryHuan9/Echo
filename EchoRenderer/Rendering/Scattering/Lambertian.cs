8a8ad40a062195779f2b51dbc388ffe7527864b8
Gary Huang <henhaodegaigai@gmail.com>
2021-10-20 00:36:52 -0400

    Rewriting materials

---
+++ EchoRenderer/Rendering/Scattering/Lambertian.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using CodeHelpers.Mathematics;
+
+namespace EchoRenderer.Rendering.Scattering
+{
+	public class LambertianReflection : BidirectionalDistributionFunction
+	{
+		public LambertianReflection() : base
+		(
+			BidirectionalDistributionFunctionType.reflection |
+			BidirectionalDistributionFunctionType.diffuse
+		) { }
+
+		public void Reset(in Float3 newReflectance) => reflectance = newReflectance;
+
+		Float3 reflectance;
+
+		public override Float3 Sample(in Float3 outgoing, in Float3 incident) => reflectance * (1f / Scalars.PI);
+
+		public override Float3 GetReflectance(in Float3            outLocal, ReadOnlySpan<Float2> samples)  => reflectance;
+		public override Float3 GetReflectance(ReadOnlySpan<Float2> samples0, ReadOnlySpan<Float2> samples1) => reflectance;
+	}
+
+	public class LambertianTransmission : BidirectionalDistributionFunction
+	{
+		public LambertianTransmission() : base
+		(
+			BidirectionalDistributionFunctionType.transmission |
+			BidirectionalDistributionFunctionType.diffuse
+		) { }
+
+		public void Reset(in Float3 newTransmittance) => transmittance = newTransmittance;
+
+		Float3 transmittance;
+
+		public override Float3 Sample(in Float3 outgoing, in Float3 incident) => transmittance * (1f / Scalars.PI);
+
+		public override Float3 GetReflectance(in Float3            outLocal, ReadOnlySpan<Float2> samples)  => transmittance;
+		public override Float3 GetReflectance(ReadOnlySpan<Float2> samples0, ReadOnlySpan<Float2> samples1) => transmittance;
+	}
+}
---
+++ EchoRenderer/Rendering/Scattering/Specular.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using CodeHelpers.Mathematics;
+
+namespace EchoRenderer.Rendering.Scattering
+{
+	public class SpecularReflection : BidirectionalDistributionFunction
+	{
+		public SpecularReflection() : base
+		(
+			BidirectionalDistributionFunctionType.reflection |
+			BidirectionalDistributionFunctionType.specular
+		) { }
+
+		public void Reset(in Float3 newReflectance, in FresnelDielectric newDielectric)
+		{
+			reflectance  = newReflectance;
+			dielectric   = newDielectric;
+			isDielectric = true;
+		}
+
+		public void Reset(in Float3 newReflectance, in FresnelConductor newConductor)
+		{
+			reflectance  = newReflectance;
+			conductor    = newConductor;
+			isDielectric = false;
+		}
+
+		Float3 reflectance;
+		bool   isDielectric;
+
+		FresnelDielectric dielectric;
+		FresnelConductor  conductor;
+
+		public override Float3 Sample(in Float3 outgoing, in Float3 incident) => Float3.zero;
+
+		public override Float3 Sample(in Float3 outgoing, out Float3 incident, in Float2 sample, out float pdf, ref BidirectionalDistributionFunctionType type)
+		{
+			incident = new Float3(-outgoing.x, -outgoing.y, outgoing.z);
+
+			pdf = 1f;
+			float cosI = Cosine(incident);
+
+			Float3 evaluation = isDielectric ? dielectric.Evaluate(cosI) : conductor.Evaluate(cosI);
+			return evaluation * reflectance / AbsoluteCosine(incident);
+		}
+	}
+
+	public class SpecularTransmission : BidirectionalDistributionFunction
+	{
+		public SpecularTransmission() : base
+		(
+			BidirectionalDistributionFunctionType.transmission |
+			BidirectionalDistributionFunctionType.specular
+		) { }
+
+		public void Reset(in Float3 newTransmittance, float newEtaAbove, float newEtaBelow)
+		{
+			transmittance = newTransmittance;
+			etaAbove      = newEtaAbove;
+			etaBelow      = newEtaBelow;
+
+			fresnel = new FresnelDielectric(etaAbove, etaBelow);
+		}
+
+		Float3 transmittance;
+		float  etaAbove;
+		float  etaBelow;
+
+		FresnelDielectric fresnel;
+
+		//TODO
+
+		public override Float3 Sample(in Float3 outgoing, in Float3 incident) => Float3.zero;
+
+		public override Float3 Sample(in Float3 outgoing, out Float3 incident, in Float2 sample, out float pdf, ref BidirectionalDistributionFunctionType type) => throw new NotImplementedException();
+	}
+}
