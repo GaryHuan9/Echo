a934e74fcc3633a2bb375076e7f895d47f9f8f6e
Gary Huang <henhaodegaigai@gmail.com>
2021-10-21 00:38:52 -0400

    Implementing material Matte

---
+++ EchoRenderer/Rendering/Materials/IMaterial.cs
@@ -0,0 +1,14 @@
+﻿using EchoRenderer.Mathematics.Intersections;
+using EchoRenderer.Rendering.Scattering;
+
+namespace EchoRenderer.Rendering.Materials
+{
+	public interface IMaterial
+	{
+		/// <summary>
+		/// Determines the scattering properties of this material at <paramref name="query"/>.
+		/// Initializes a <see cref="BidirectionalScatteringDistributionFunctions"/>.
+		/// </summary>
+		void Scatter(ref HitQuery query, MemoryArena arena, TransportMode mode);
+	}
+}
