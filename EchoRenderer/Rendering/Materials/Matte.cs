b922e2eec07f56007b07a0b90ad9479fbde39490
Gary Huang <henhaodegaigai@gmail.com>
2021-10-21 00:38:52 -0400

    Implementing material Matte
    Former-commit-id: 7b02e978232afe12bbf401d945cadf3544c5cc28

---
+++ EchoRenderer/Rendering/Materials/Matte.cs
@@ -0,0 +1,13 @@
+﻿using EchoRenderer.Mathematics.Intersections;
+
+namespace EchoRenderer.Rendering.Materials
+{
+	public class Matte : IMaterial
+	{
+
+		public void Scatter(ref HitQuery query, MemoryArena arena, TransportMode mode)
+		{
+
+		}
+	}
+}
