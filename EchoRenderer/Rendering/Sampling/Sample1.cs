1157f4e0c872e1bba059d13d5725cf441c87dea0
Gary Huang <henhaodegaigai@gmail.com>
2021-12-02 19:59:00 -0500

    Adding sampler

--- EchoRenderer/Rendering/Sampling/Sample1.cs
+++ EchoRenderer/Rendering/Sampling/Sample1.cs
@@ -1,7 +1,16 @@
-﻿namespace EchoRenderer.Rendering.Sampling
+﻿using CodeHelpers.Diagnostics;
+
+namespace EchoRenderer.Rendering.Sampling
 {
 	public readonly struct Sample1
 	{
-		
+		public Sample1(float u)
+		{
+			Assert.IsTrue(0f <= u);
+			Assert.IsTrue(1f > u);
+			this.u = u;
+		}
+
+		public readonly float u;
 	}
 }
