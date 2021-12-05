b0cbf19b1ee3b0a8733608a58088ac8ec50387fc
Gary Huang <henhaodegaigai@gmail.com>
2021-12-05 01:10:24 -0500

    Added StratifiedSampler
    Former-commit-id: a10f57ab65fe0d51912ca207d56a3a299342b61d

---
+++ EchoRenderer/Rendering/Sampling/LimitedSampler.cs
@@ -0,0 +1,40 @@
+﻿namespace EchoRenderer.Rendering.Sampling
+{
+	public abstract class LimitedSampler : Sampler
+	{
+		protected LimitedSampler(int sampleCount, int dimensionCount) : base(sampleCount)
+		{
+			ones = new SpanAggregate<Sample1>[dimensionCount];
+			twos = new SpanAggregate<Sample2>[dimensionCount];
+
+			for (int i = 0; i < dimensionCount; i++)
+			{
+				ones[i] = new SpanAggregate<Sample1>(1, sampleCount);
+				twos[i] = new SpanAggregate<Sample2>(1, sampleCount);
+			}
+		}
+
+		protected LimitedSampler(LimitedSampler sampler) : base(sampler)
+		{
+			ones = new SpanAggregate<Sample1>[sampler.ones.Length];
+			twos = new SpanAggregate<Sample2>[sampler.twos.Length];
+		}
+
+		protected readonly SpanAggregate<Sample1>[] ones;
+		protected readonly SpanAggregate<Sample2>[] twos;
+
+		int oneIndex;
+		int twoIndex;
+
+		public override void BeginSample()
+		{
+			base.BeginSample();
+
+			oneIndex = -1;
+			twoIndex = -1;
+		}
+
+		public override Sample1 NextOne() => ones[++oneIndex].array[SampleIndex];
+		public override Sample2 NextTwo() => twos[++twoIndex].array[SampleIndex];
+	}
+}
--- EchoRenderer/Rendering/Sampling/Sample1.cs
+++ EchoRenderer/Rendering/Sampling/Sample1.cs
@@ -1,15 +1,10 @@
-﻿using CodeHelpers.Diagnostics;
+﻿using EchoRenderer.Mathematics;
 
 namespace EchoRenderer.Rendering.Sampling
 {
 	public readonly struct Sample1
 	{
-		public Sample1(float u)
-		{
-			Assert.IsTrue(0f <= u);
-			Assert.IsTrue(1f > u);
-			this.u = u;
-		}
+		public Sample1(float u) => this.u = FastMath.ClampEpsilon(u);
 
 		public readonly float u;
 	}
