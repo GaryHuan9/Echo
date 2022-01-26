dc0069ed374feb4ee01cbc51ecce80f038f19a01
Gary Huang <henhaodegaigai@gmail.com>
2022-01-26 00:44:23 -0500

    Completed sampling methods in Sphere
    Former-commit-id: a5010ed927fbb6867a47ecda4011441c2616e81a

--- EchoRenderer/src/Rendering/Materials/Material.cs
+++ EchoRenderer/src/Rendering/Materials/Material.cs
@@ -1,4 +1,6 @@
 ﻿using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+using CodeHelpers;
 using CodeHelpers.Mathematics;
 using EchoRenderer.Common;
 using EchoRenderer.Mathematics;
@@ -12,21 +14,53 @@ namespace EchoRenderer.Rendering.Materials
 
 	public abstract class Material
 	{
-		public Texture Albedo { get; set; }
-		public Texture Normal { get; set; }
+		NotNull<Texture> _albedo = Texture.black;
+		NotNull<Texture> _normal = Texture.normal;
 
-		public float NormalIntensity { get; set; } = 1f;
+		/// <summary>
+		/// The primary color of this <see cref="Material"/>.
+		/// </summary>
+		public Texture Albedo
+		{
+			get => _albedo;
+			set => _albedo = value;
+		}
+
+		/// <summary>
+		/// The local normal direction deviation of this <see cref="Material"/>.
+		/// </summary>
+		public Texture Normal
+		{
+			get => _normal;
+			set => _normal = value;
+		}
+
+		/// <summary>
+		/// The emission of this <see cref="Material"/>.
+		/// </summary>
+		public Float3 Emission { get; set; }
+
+		/// <summary>
+		/// The intensity of <see cref="Normal"/> on this <see cref="Material"/>.
+		/// </summary>
+		public Float3 NormalIntensity { get; set; } = Float3.one;
+
+		bool zeroEmission;
+		bool zeroNormal;
+
+		Vector128<float> normalIntensityVector;
 
 		static Vector128<float> NormalShift => Vector128.Create(-1f, -1f, -2f, 0f);
 
 		/// <summary>
-		/// Invoked before a new render session begins; can be used to execute
-		/// any kind of preprocessing work for this <see cref="Material"/>.
+		/// Invoked before a new render session begins; can be used to execute any kind of preprocessing work for this <see cref="Material"/>.
+		/// NOTE: invoking any of the rendering related methods prior to invoking this method after a change will result in undefined behaviors!
 		/// </summary>
 		public virtual void Prepare()
 		{
-			Albedo ??= Texture.black;
-			Normal ??= Texture.normal;
+			zeroEmission = !Emission.PositiveRadiance();
+			zeroNormal = Normal == Texture.normal || NormalIntensity == Float3.zero;
+			normalIntensityVector = Utilities.ToVector(NormalIntensity);
 		}
 
 		/// <summary>
@@ -41,21 +75,20 @@ namespace EchoRenderer.Rendering.Materials
 		/// </summary>
 		public bool ApplyNormalMapping(in Float2 texcoord, ref Float3 normal)
 		{
-			if (Normal == Texture.normal || NormalIntensity.AlmostEquals()) return false;
+			if (zeroNormal) return false;
 
 			//Evaluate normal texture at texcoord
-			Vector128<float> sample = PackedMath.Clamp01(Normal[texcoord]);
-			Vector128<float> local = PackedMath.FMA(sample, Vector128.Create(2f), NormalShift);
+			Vector128<float> local = PackedMath.Clamp01(Normal[texcoord]);
+			local = PackedMath.FMA(local, Vector128.Create(2f), NormalShift);
 
+			local = Sse.Multiply(local, normalIntensityVector);
 			if (PackedMath.AlmostZero(local)) return false;
 
 			//Create transform to move from local direction to world space based
 			NormalTransform transform = new NormalTransform(normal);
-			Float3 delta = transform.LocalToWorld(Utilities.ToFloat3(ref local));
+			Float3 delta = transform.LocalToWorld(Utilities.ToFloat3(local));
 
-			normal -= delta * NormalIntensity;
-			normal = normal.Normalized;
-
+			normal = (normal - delta).Normalized;
 			return true;
 		}
 
