bba71cc2d87a247c3a944808c4df68b7bf8b5451
Gary Huang <henhaodegaigai@gmail.com>
2022-01-27 17:36:19 -0500

    Point lights match with Cycles groundtruth!
    Former-commit-id: 876992a3461f9da657db170e0a5739bf269b8e57

--- EchoRenderer/src/Objects/Scenes/Scenes.cs
+++ EchoRenderer/src/Objects/Scenes/Scenes.cs
@@ -1,7 +1,6 @@
 ﻿using CodeHelpers;
 using CodeHelpers.Mathematics;
 using CodeHelpers.Mathematics.Enumerable;
-using EchoRenderer.Common;
 using EchoRenderer.IO;
 using EchoRenderer.Objects.GeometryObjects;
 using EchoRenderer.Objects.Lights;
@@ -18,19 +17,18 @@ namespace EchoRenderer.Objects.Scenes
 		{
 			var mesh = new Mesh("Assets/Models/StanfordBunny/lowPoly.obj");
 			// var materials = new MaterialLibrary("Assets/Models/StanfordBunny/bunny.mat");
-			// var material0 = new Matte { Albedo = (Pure)new Float3(1f, 0.68f, 0.16f) };
-			// var material1 = new Matte { Albedo = (Pure)Utilities.ToColor("0x06245F") };
-			var material1 = new Matte { Albedo = Texture.white };
+			var material0 = new Matte { Albedo = (Pure)new Float3(1f, 0.68f, 0.16f) };
+			var material1 = new Matte { Albedo = (Pure)new Float3(0.0250f, 0.1416f, 0.3736f) };
 
 			// children.Add(new AmbientLight { Texture = new Cubemap("Assets/Cubemaps/OutsideDayTime") });
 			children.Add(new AmbientLight { Texture = new CylindricalTexture { Texture = TextureGrid.Load("Assets/Cubemaps/UlmerMuenster.jpg") } });
 
-			children.Add(new MeshObject(mesh, material1) { Position = new Float3(0f, 0f, -3f), Rotation = new Float3(0f, 180f, 0f), Scale = (Float3)2.5f });
+			children.Add(new MeshObject(mesh, material0) { Position = new Float3(0f, 0f, -3f), Rotation = new Float3(0f, 180f, 0f), Scale = (Float3)2.5f });
 
 			children.Add(new SphereObject(material1, 1f) { Position = new Float3(-3f, 1f, -2f) });
 
-			// children.Add(new PointLight { Intensity = new Float3(20f, 10f, 10f), Position = new Float3(2f, 2f, -6f) });
-			// children.Add(new PointLight { Intensity = new Float3(10f, 10f, 10f), Position = new Float3(-3f, 3f, -4f) });
+			children.Add(new PointLight { Intensity = new Float3(20f, 10f, 10f), Position = new Float3(2f, 2f, -6f) });
+			children.Add(new PointLight { Intensity = new Float3(10f, 10f, 10f), Position = new Float3(-3f, 3f, -4f) });
 		}
 	}
 
