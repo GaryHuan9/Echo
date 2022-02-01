eedf135e859d972201a2151805b155afb78f16d9
Gary Huang <henhaodegaigai@gmail.com>
2022-02-01 01:41:36 -0500

    Added caching to SwatchExtractor

--- EchoRenderer/src/Objects/Instancing/MaterialSwatch.cs
+++ EchoRenderer/src/Objects/Instancing/MaterialSwatch.cs
@@ -7,18 +7,31 @@ using EchoRenderer.Rendering.Materials;
 
 namespace EchoRenderer.Objects.Instancing
 {
+	/// <summary>
+	/// A swatch of <see cref="Material"/>s that can be used to optionally map some original materials to different alternative materials in a <see cref="PackInstance"/>.
+	/// Note that it can only exchange materials defined on geometries in the immediate <see cref="ObjectPack"/> and cannot be used to modify materials in subsequent instances!
+	/// </summary>
 	public class MaterialSwatch
 	{
 		public MaterialSwatch() => map = new Dictionary<Material, Material>();
 
 		public MaterialSwatch(MaterialSwatch source) => map = source.map.ToDictionary(pair => pair.Key, pair => pair.Value);
 
-		//NOTE: This swatch can only exchange materials defined on geometries in the immediate ObjectPack, it cannot be used to modify materials in subsequent instances!
-
 		readonly Dictionary<Material, Material> map;
 
+		/// <summary>
+		/// A simple hash for <see cref="map"/>.
+		/// Used for fast comparison and hashing.
+		/// </summary>
 		uint hash;
 
+		public static readonly ValueEqualityComparer valueEqualityComparer = new();
+
+		/// <summary>
+		/// Accesses this <see cref="MaterialSwatch"/> at <paramref name="material"/>. To map a new <see cref="Material"/>,
+		/// simply index and assign it to a different <see cref="Material"/>. A mapping can be removed by assigning either
+		/// null or the <see cref="Material"/> itself to a <see cref="Material"/> index.
+		/// </summary>
 		public Material this[Material material]
 		{
 			get => map.TryGetValue(material);
@@ -52,26 +65,30 @@ namespace EchoRenderer.Objects.Instancing
 			}
 		}
 
-		public bool Equals(MaterialSwatch other)
-		{
-			if (ReferenceEquals(this, other)) return true;
-			if (map.Count == 0 && other is null) return true;
-
-			if (hash != other.hash || map.Count != other.map.Count) return false;
-
-			foreach ((Material key, Material value) in other.map)
-			{
-				if (map.TryGetValue(key) != value) return false;
-			}
-
-			return true;
-		}
-
 		static uint Hash(Material material) => SquirrelRandom.Mangle((uint)material.GetHashCode());
 
-		public class EqualityComparer : IEqualityComparer<MaterialSwatch>
+		/// <summary>
+		/// A <see cref="IEqualityComparer{T}"/> that compares the actual value/content of two different <see cref="MaterialSwatch"/> (rather than just references).
+		/// NOTE: if the value of a <see cref="MaterialSwatch"/> is changed, then obviously the results of this <see cref="IEqualityComparer{T}"/> can change as well.
+		/// </summary>
+		public class ValueEqualityComparer : IEqualityComparer<MaterialSwatch>
 		{
-			public bool Equals(MaterialSwatch x, MaterialSwatch y) => !ReferenceEquals(x, null) && x.Equals(y);
+			public bool Equals(MaterialSwatch x, MaterialSwatch y)
+			{
+				if (ReferenceEquals(x, y)) return true;
+
+				if (ReferenceEquals(x, null)) return y.map.Count == 0;
+				if (ReferenceEquals(y, null)) return x.map.Count == 0;
+
+				if (x.hash != y.hash || x.map.Count != y.map.Count) return false;
+
+				foreach ((Material key, Material value) in y.map)
+				{
+					if (x.map.TryGetValue(key) != value) return false;
+				}
+
+				return true;
+			}
 
 			public int GetHashCode(MaterialSwatch swatch) => (int)swatch.hash;
 		}
