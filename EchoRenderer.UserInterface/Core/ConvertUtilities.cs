6280db0786fc4b756be8955ee0b0380de55dec25
Gary Huang <henhaodegaigai@gmail.com>
2022-02-04 01:11:39 -0500

    Renamed echo UI to UserInterface

---
+++ EchoRenderer.UserInterface/Core/ConvertUtilities.cs
@@ -0,0 +1,20 @@
+﻿using CodeHelpers.Mathematics;
+using SFML.System;
+using SFML.Window;
+
+namespace EchoRenderer.UserInterface.Core
+{
+	public static class ConvertUtilities
+	{
+		public static Vector2f As(this Float2 value) => new Vector2f(value.x, value.y);
+		public static Vector2i As(this Int2 value) => new Vector2i(value.x, value.y);
+		public static Vector2u Cast(this Int2 value) => new Vector2u((uint)value.x, (uint)value.y);
+
+		public static Float2 As(this Vector2f value) => new Float2(value.X, value.Y);
+		public static Int2 As(this Vector2i value) => new Int2(value.X, value.Y);
+		public static Int2 Cast(this Vector2u value) => new Int2((int)value.X, (int)value.Y);
+
+		public static Int2 As(this MouseMoveEventArgs args) => new Int2(args.X, args.Y);
+		public static Int2 As(this MouseButtonEventArgs args) => new Int2(args.X, args.Y);
+	}
+}
---
+++ EchoRenderer.UserInterface/Interface/HierarchyUI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using CodeHelpers.Diagnostics;
+using EchoRenderer.Scenic;
+using EchoRenderer.Scenic.Instancing;
+using EchoRenderer.Scenic.Preparation;
+using EchoRenderer.UserInterface.Core.Areas;
+
+namespace EchoRenderer.UserInterface.Interface
+{
+	public class HierarchyUI : WindowUI
+	{
+		public HierarchyUI() : base("Hierarchy")
+		{
+			transform.RightPercent = 0.84f;
+
+			rebuildButton = new ButtonUI {label = {Text = "Rebuild Scene"}};
+			rebuildButton.OnPressedMethods += OnRebuildPressed;
+
+			group.Add(rebuildButton);
+		}
+
+		readonly ButtonUI rebuildButton;
+
+		readonly HashSet<EntityPack> packs = new();
+		readonly List<HierarchyNodeUI> nodes = new();
+
+		public override void Update()
+		{
+			base.Update();
+
+			SceneViewUI sceneView = Root.Find<SceneViewUI>();
+			Scene scene = sceneView?.Profile.Scene?.source;
+
+			if (scene == null || packs.Contains(scene)) return;
+
+			foreach (HierarchyNodeUI node in nodes) group.Remove(node);
+
+			packs.Clear();
+			nodes.Clear();
+
+			ScenePreparer preparer = sceneView.Profile.Scene.preparer;
+
+			foreach (EntityPack pack in preparer.UniquePacks)
+			{
+				Assert.IsFalse(packs.Contains(pack));
+				var node = new HierarchyNodeUI(pack);
+
+				packs.Add(pack);
+				nodes.Add(node);
+				group.Add(node);
+			}
+		}
+
+		void OnRebuildPressed() { }
+	}
+}
