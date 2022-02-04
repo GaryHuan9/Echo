ced847d036108d35674c883d425a7dc1e7e1264f
Gary Huang <henhaodegaigai@gmail.com>
2022-02-04 01:11:39 -0500

    Renamed echo UI to UserInterface

---
+++ EchoRenderer.UserInterface/Core/Fields/FloatsFieldUI.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using EchoRenderer.UserInterface.Core.Areas;
+
+namespace EchoRenderer.UserInterface.Core.Fields
+{
+	public class FloatsFieldUI : AutoLayoutAreaUI
+	{
+		public FloatsFieldUI(int length)
+		{
+			Horizontal = true;
+			Margins = false;
+
+			values = new Values(this);
+			fields = new FloatFieldUI[length];
+
+			for (int i = 0; i < length; i++)
+			{
+				var field = new FloatFieldUI();
+
+				field.OnValueChangedMethods += OnValueChanged;
+
+				Add(fields[i] = field);
+			}
+		}
+
+		public readonly Values values;
+		public int Length => fields.Length;
+
+		public event Action<FloatsFieldUI> OnValuesChangedMethods;
+
+		readonly FloatFieldUI[] fields;
+
+		void OnValueChanged(FloatFieldUI field)
+		{
+			OnValuesChangedMethods?.Invoke(this);
+		}
+
+		public class Values
+		{
+			public Values(FloatsFieldUI field) => this.field = field;
+
+			readonly FloatsFieldUI field;
+
+			public float this[int index]
+			{
+				get => field.fields[index].Value;
+				set => field.fields[index].Value = value;
+			}
+		}
+	}
+}
---
+++ EchoRenderer.UserInterface/Interface/RenderPreviewUI.cs
@@ -0,0 +1,57 @@
+﻿using EchoRenderer.Textures.Grid;
+using EchoRenderer.UserInterface.Core;
+using EchoRenderer.UserInterface.Core.Areas;
+using SFML.Graphics;
+
+namespace EchoRenderer.UserInterface.Interface
+{
+	public class RenderPreviewUI : AreaUI
+	{
+		public RenderPreviewUI()
+		{
+			imageUI = new ImageUI {KeepAspect = false};
+
+			Add(imageUI);
+		}
+
+		ProgressiveRenderBuffer _renderBuffer;
+
+		public ProgressiveRenderBuffer RenderBuffer
+		{
+			get => _renderBuffer;
+			set
+			{
+				if (_renderBuffer == value) return;
+				Texture texture = imageUI.Texture;
+
+				if (texture != null && texture.Size.Cast() != value.size)
+				{
+					texture.Dispose();
+					texture = null;
+				}
+
+				if (texture == null)
+				{
+					uint width = (uint)value.size.x;
+					uint height = (uint)value.size.y;
+
+					imageUI.Texture = new Texture(width, height);
+				}
+
+				_renderBuffer = value;
+			}
+		}
+
+		readonly ImageUI imageUI;
+
+		public override void Update()
+		{
+			base.Update();
+
+			var buffer = RenderBuffer;
+			if (buffer == null) return;
+
+			imageUI.Texture.Update(buffer.bytes);
+		}
+	}
+}
