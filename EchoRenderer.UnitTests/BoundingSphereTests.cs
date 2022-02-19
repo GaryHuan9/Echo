42df6fa5d1018c28ef3a643a26a7750f40a0d78e
Gary Huang <henhaodegaigai@gmail.com>
2022-02-19 18:22:05 -0500

    Refactored BoundingSphere

---
+++ EchoRenderer.UnitTests/BoundingSphereTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CodeHelpers.Mathematics;
+using EchoRenderer.Common.Mathematics.Primitives;
+using EchoRenderer.Common.Mathematics.Randomization;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace EchoRenderer.UnitTests;
+
+public class BoundingSphereTests
+{
+	[SetUp]
+	[Repeat(100)]
+	public void Setup()
+	{
+		points = GenerateRandomPoints();
+		sphere = new BoundingSphere(points);
+	}
+
+	[Test]
+	public void ContainsAll()
+	{
+		foreach (Float3 point in points) Assert.That(sphere.Contains(point));
+	}
+
+	Float3[] points;
+	BoundingSphere sphere;
+
+	static Float3[] GenerateRandomPoints()
+	{
+		IRandom random = new SquirrelRandom(TestContext.CurrentContext.Random.NextUInt());
+
+		var points = new Float3[random.Next1(1000)];
+		FillRandom(points, random.Next1(), random);
+
+		return points;
+	}
+
+	static void FillRandom(Span<Float3> points, float maxRadius, IRandom random)
+	{
+		foreach (ref Float3 point in points) point = random.NextInSphere(maxRadius);
+	}
+}
