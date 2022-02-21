1f3b225f28e1267248748e77691205d809f3bfcb
Gary Huang <henhaodegaigai@gmail.com>
2022-02-21 00:18:30 -0500

    Added CylindricalTextureTests

---
+++ EchoRenderer.UnitTests/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using EchoRenderer.Common.Mathematics.Randomization;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace EchoRenderer.UnitTests;
+
+public static class Utilities
+{
+	/// <summary>
+	/// Returns a new <see cref="IRandom"/> for this current <see cref="TestContext"/>.
+	/// </summary>
+	public static IRandom NewRandom() => new SquirrelRandom(TestContext.CurrentContext.Random.NextUInt());
+
+	/// <inheritdoc cref="EqualConstraint.Ulps"/>
+	public static EqualConstraint Roughly(this EqualConstraint constraint, int ulps = 3) => constraint.Within(ulps).Ulps;
+}
