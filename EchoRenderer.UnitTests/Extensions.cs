7c2a1b1c5bc0c2872ca1b7ff293f77a0ca67e560
Gary Huang <henhaodegaigai@gmail.com>
2022-02-21 00:18:30 -0500

    Added CylindricalTextureTests

--- EchoRenderer.UnitTests/Extensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using NUnit.Framework.Constraints;
-
-namespace EchoRenderer.UnitTests;
-
-public static class Extensions
-{
-	/// <inheritdoc cref="EqualConstraint.Ulps"/>
-	public static EqualConstraint Roughly(this EqualConstraint constraint, int ulps = 3) => constraint.Within(ulps).Ulps;
-}
