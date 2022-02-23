e0038f34d669c040b77c7a741ce7bf731765a8d4
Gary Huang <henhaodegaigai@gmail.com>
2022-02-23 11:25:21 -0500

    Made suggestions to View and ROView
    Former-commit-id: 7ed7156b1d497b6aef4ec2a70ac5553d54964337

--- EchoRenderer.UnitTests/ViewTests.cs
+++ EchoRenderer.UnitTests/ViewTests.cs
@@ -8,72 +8,69 @@ namespace EchoRenderer.UnitTests;
 public class ViewTests
 {
 	[SetUp]
-	public void Setup()
+	public void SetUp()
 	{
 		// [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 		data = Enumerable.Range(1, 10).ToArray();
-		dataView = data.AsView();
+		view = data.AsView();
+	}
+
+	int[] data;
+	View<int> view;
+
+	[Test]
+	public void Conversion()
+	{
+		Assert.That(data.Length, Is.EqualTo(view.Length));
+		Assert.That(data[0], Is.EqualTo(view[0]));
+		Assert.That(data[^1], Is.EqualTo(view[^1]));
 	}
 
 	[Test]
-	public void ConversionTest()
+	public void ReadOnlyConversion()
 	{
-		Assert.That(data.Length, Is.EqualTo(dataView.count));
-		Assert.That(data[0], Is.EqualTo(dataView[0]));
-		Assert.That(data[^1], Is.EqualTo(dataView[^1]));
-	}
-	
-	[Test]
-	public void ReadonlyConversionTest()
-	{
-		ReadOnlyView<int> readOnlyView = dataView;
-		
-		Assert.That(data.Length, Is.EqualTo(readOnlyView.count));
+		ReadOnlyView<int> readOnlyView = view;
+
+		Assert.That(data.Length, Is.EqualTo(readOnlyView.Length));
 		Assert.That(data[0], Is.EqualTo(readOnlyView[0]));
 		Assert.That(data[^1], Is.EqualTo(readOnlyView[^1]));
 	}
 
 	[Test]
-	public void SliceTest()
+	public void Slice()
 	{
 		// expected result [ 2, 3 ]
-		View<int> slicedViewWithLength = dataView.Slice(1, 2);
+		View<int> slicedViewWithLength = view.Slice(1, 2);
 
-		Assert.That(slicedViewWithLength.count, Is.EqualTo(2));
+		Assert.That(slicedViewWithLength.Length, Is.EqualTo(2));
 		Assert.That(slicedViewWithLength[0], Is.EqualTo(2));
 		Assert.That(slicedViewWithLength[^1], Is.EqualTo(3));
-		
-		
-		// expected result [ 6, 7, 8, 9, 10 ]
-		View<int> slicedViewNoLength = dataView.Slice(5);
 
-		Assert.That(slicedViewNoLength.count, Is.EqualTo(5));
+
+		// expected result [ 6, 7, 8, 9, 10 ]
+		View<int> slicedViewNoLength = view[5..];
+
+		Assert.That(slicedViewNoLength.Length, Is.EqualTo(5));
 		Assert.That(slicedViewNoLength[0], Is.EqualTo(6));
 		Assert.That(slicedViewNoLength[^1], Is.EqualTo(10));
-		
-		
 	}
 
 	[Test]
-	public void ReadonlySliceTest()
+	public void ReadOnlySlice()
 	{
 		// expected result [ 2, 3 ]
-		ReadOnlyView<int> roSlicedViewWithLength = dataView.Slice(1, 2);
-		
-		Assert.That(roSlicedViewWithLength.count, Is.EqualTo(2));
+		ReadOnlyView<int> roSlicedViewWithLength = view.Slice(1, 2);
+
+		Assert.That(roSlicedViewWithLength.Length, Is.EqualTo(2));
 		Assert.That(roSlicedViewWithLength[0], Is.EqualTo(2));
 		Assert.That(roSlicedViewWithLength[^1], Is.EqualTo(3));
-		
-		
+
+
 		// expected result [ 6, 7, 8, 9, 10 ]
-		ReadOnlyView<int> roSlicedVewNoLength = dataView.Slice(5);
-		
-		Assert.That(roSlicedVewNoLength.count, Is.EqualTo(5));
+		ReadOnlyView<int> roSlicedVewNoLength = view[5..];
+
+		Assert.That(roSlicedVewNoLength.Length, Is.EqualTo(5));
 		Assert.That(roSlicedVewNoLength[0], Is.EqualTo(6));
 		Assert.That(roSlicedVewNoLength[^1], Is.EqualTo(10));
 	}
-
-	int[] data;
-
-	View<int> dataView;
 }
