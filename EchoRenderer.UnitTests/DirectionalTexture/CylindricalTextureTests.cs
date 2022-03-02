c54f2195aa5e17f969b1cb898b1215596861fa54
Gary Huang <henhaodegaigai@gmail.com>
2022-03-01 21:14:24 -0500

    Improved Unit Tests and slightly changed View

--- EchoRenderer.UnitTests/DirectionalTexture/CylindricalTextureTests.cs
+++ EchoRenderer.UnitTests/DirectionalTexture/CylindricalTextureTests.cs
@@ -2,10 +2,10 @@
 using EchoRenderer.Core.Texturing.Directional;
 using NUnit.Framework;
 
-namespace EchoRenderer.UnitTests;
+namespace EchoRenderer.UnitTests.DirectionalTexture;
 
 [TestFixture]
-public class CylindricalTextureTests : IDirectionalTextureTests
+public class CylindricalTextureTests : DirectionalTextureBaseTests
 {
 	protected override IDirectionalTexture GetTexture() => new CylindricalTexture { Texture = GenerateRandomTexture((Int2)500) };
 }
