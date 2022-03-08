2c28a857b6bb18e59428476c023a5fa4302d1c5e
CXRedix <1135058679@qq.com>
2022-03-08 12:08:32 +0800

    Added and fixed comments for View & ROView

--- EchoRenderer/src/Common/Memory/ReadOnlyView.cs
+++ EchoRenderer/src/Common/Memory/ReadOnlyView.cs
@@ -20,50 +20,64 @@ public readonly struct ReadOnlyView<T>
 	}
 
 	/// <summary>
-	///		Returns an empty <see cref="ReadOnlyView{T}"/> object
+	///	Returns an empty <see cref="ReadOnlyView{T}"/> object.
 	/// </summary>
 	public static ReadOnlyView<T> Empty => default;
 
+	/// <summary>
+	/// Returns a readonly reference to the specified element of the View.
+	/// </summary>
 	public ref readonly T this[int index] => ref array[AssertShift(index)];
 
 	/// <summary>
-	///		Returns a value which indicates whether this view is empty
+	///	Returns a value which indicates whether this view is empty.
 	/// </summary>
 	public bool IsEmpty => Length == 0;
 
 	/// <summary>
-	///		Returns the length of the current view
+	///	Returns the length of the current view.
 	/// </summary>
 	public int Length { get; }
 
 	readonly T[] array;
 	readonly int start;
 
+	/// <summary>
+	/// Returns an enumerator for this View.
+	/// </summary>
 	public ReadOnlySpan<T>.Enumerator GetEnumerator() => AsSpan().GetEnumerator();
 
 	/// <summary>
-	///		Forms a slice out of the given readonly view, beginning at '<paramref name="offset"/>'
+	///	Forms a slice out of the given readonly view, beginning at '<paramref name="offset"/>'.
 	/// </summary>
 	public ReadOnlyView<T> Slice(int offset) => Slice(offset, Length - offset);
 
 	/// <inheritdoc cref="Slice(int)"/>
 	public ReadOnlyView<T> Slice(int offset, int length) => new(array, AssertShift(offset), length);
 
+	/// <summary>
+	/// Converts the current view into a <see cref="ReadOnlySpan{T}"/>.
+	/// </summary>
 	public ReadOnlySpan<T> AsSpan() => array.AsSpan(start, Length);
+	
+	/// <inheritdoc cref="AsSpan()"/>
 	public ReadOnlySpan<T> AsSpan(int offset) => array.AsSpan(AssertShift(offset), Length);
+	/// <inheritdoc cref="AsSpan()"/>
 	public ReadOnlySpan<T> AsSpan(int offset, int length) => array.AsSpan(AssertShift(offset), length);
 	
+	/// <inheritdoc cref="AsSpan()"/>
 	public ReadOnlySpan<T> AsSpan(Range range)
 	{
 		(int offset, int length) = range.GetOffsetAndLength(Length);
 		return AsSpan(offset, length);
 	}
 
+	/// <inheritdoc cref="AsSpan()"/>
 	public ReadOnlySpan<T> AsSpan(Index startIndex) => AsSpan(startIndex.GetOffset(Length));
 
 	/// <summary>
-	///     Asserts and Shifts the view array index to the original array index
-	///     if the <paramref name="index" /> is less than <see cref="Length" />
+	/// Asserts and Shifts the view array index to the original array index,
+	/// if the <paramref name="index" /> is less than <see cref="Length" />.
 	/// </summary>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	int AssertShift(int index)
--- EchoRenderer/src/Common/Memory/View.cs
+++ EchoRenderer/src/Common/Memory/View.cs
@@ -20,50 +20,64 @@ public readonly struct View<T>
 	}
 
 	/// <summary>
-	///		Returns an empty <see cref="View{T}"/> object
+	///	Returns an empty <see cref="View{T}"/> object.
 	/// </summary>
 	public static View<T> Empty => default;
 
+	/// <summary>
+	/// Returns a reference to the specified element of the View.
+	/// </summary>
 	public ref T this[int index] => ref array[AssertShift(index)];
 
 	/// <summary>
-	///		Returns a value which indicates whether this view is empty
+	///	Returns a value which indicates whether this view is empty.
 	/// </summary>
 	public bool IsEmpty => Length == 0;
 
 	/// <summary>
-	///		Represents the length of the current view 
+	///	Represents the length of the current view .
 	/// </summary>
 	public int Length { get; }
 
 	readonly T[] array;
 	readonly int start;
 
+	/// <summary>
+	/// Returns an enumerator for this View.
+	/// </summary>
 	public Span<T>.Enumerator GetEnumerator() => AsSpan().GetEnumerator();
 
 	/// <summary>
-	///		Forms a slice out of the given view, beginning at '<paramref name="offset"/>'
+	///	Forms a slice out of the given view, beginning at '<paramref name="offset"/>'.
 	/// </summary>
 	public View<T> Slice(int offset) => Slice(offset, Length - offset);
 
 	/// <inheritdoc cref="Slice(int)"/>
 	public View<T> Slice(int offset, int length) => new(array, AssertShift(offset), length);
 
+	/// <summary>
+	/// Converts the current view into a <see cref="Span{T}"/>.
+	/// </summary>
 	public Span<T> AsSpan() => array.AsSpan(start, Length);
+	
+	/// <inheritdoc cref="AsSpan()"/>
 	public Span<T> AsSpan(int offset) => array.AsSpan(AssertShift(offset));
+	/// <inheritdoc cref="AsSpan()"/>
 	public Span<T> AsSpan(int offset, int length) => array.AsSpan(AssertShift(offset), length);
 
+	/// <inheritdoc cref="AsSpan()"/>
 	public Span<T> AsSpan(Range range)
 	{
 		(int offset, int length) = range.GetOffsetAndLength(Length);
 		return AsSpan(offset, length);
 	}
 
+	/// <inheritdoc cref="AsSpan()"/>
 	public Span<T> AsSpan(Index startIndex) => AsSpan(startIndex.GetOffset(Length));
 
 	/// <summary>
-	///     Asserts and Shifts the view array index to the original array index
-	///     if the <paramref name="index" /> is less than <see cref="Length" />
+	/// Asserts and Shifts the view array index to the original array index,
+	/// if the <paramref name="index" /> is less than <see cref="Length" />.
 	/// </summary>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	int AssertShift(int index)
