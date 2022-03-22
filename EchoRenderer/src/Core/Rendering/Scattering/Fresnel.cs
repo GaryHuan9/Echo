8216993fddcacd7157f6c4764bf4270f0004341c
Gary Huang <henhaodegaigai@gmail.com>
2022-03-22 01:54:48 -0400

    Added specular

--- EchoRenderer/src/Core/Rendering/Scattering/Fresnel.cs
+++ EchoRenderer/src/Core/Rendering/Scattering/Fresnel.cs
@@ -1,92 +1,158 @@
-﻿using System;
-using CodeHelpers;
+﻿using System.Runtime.CompilerServices;
 using CodeHelpers.Mathematics;
+using EchoRenderer.Common;
 using EchoRenderer.Common.Mathematics;
 
 namespace EchoRenderer.Core.Rendering.Scattering;
 
 public readonly struct FresnelDielectric
 {
-	public FresnelDielectric(float newEtaI, float newEtaT)
+	public FresnelDielectric(float etaAbove, float etaBelow)
 	{
-		fresnelEtaI = newEtaI;
-		fresnelEtaT = newEtaT;
+		this.etaAbove = etaAbove;
+		this.etaBelow = etaBelow;
 	}
 
-	readonly float fresnelEtaI;
-	readonly float fresnelEtaT;
+	readonly float etaAbove;
+	readonly float etaBelow;
 
 	public Float3 Evaluate(float cosI)
 	{
-		float etaI = fresnelEtaI;
-		float etaT = fresnelEtaT;
+		//Get the indices of reflection
+		GetIndices(ref cosI, out float etaI, out float etaT);
 
+		//Apply Snell's law
+		if (GetCosineTransmittance(cosI, etaI / etaT, out float cosT)) return Float3.one;
+
+		//Fresnel equation
+		return (Float3)Apply(cosI, cosT, etaI, etaT);
+	}
+
+	public Float3 Evaluate(in Float3 incoming, out Float3 transmit)
+	{
+		//Get the indices of reflection
+		float cosI = BxDF.CosineP(incoming);
+		GetIndices(ref cosI, out float etaI, out float etaT);
+
+		//Apply Snell's law
+		float eta = etaI / etaT;
+
+		if (GetCosineTransmittance(cosI, eta, out float cosT))
+		{
+			transmit = default;
+			return Float3.one;
+		}
+
+		//Fresnel equation
+		Float3 normal = BxDF.Normal(incoming);
+		transmit = (eta * cosI - cosT) * normal - eta * incoming;
+		transmit = transmit.Normalized;
+
+		return (Float3)Apply(cosI, cosT, etaI, etaT);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	void GetIndices(ref float cosI, out float etaI, out float etaT)
+	{
+		etaI = etaAbove;
+		etaT = etaBelow;
 		cosI = FastMath.Clamp11(cosI);
 
 		//Swap indices of refraction if needed
-		if (cosI < 0f)
+		if (cosI >= 0f) return;
+
+		etaI = etaBelow;
+		etaT = etaAbove;
+		cosI = -cosI;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	static bool GetCosineTransmittance(float cosI, float eta, out float cosT)
+	{
+		float sinT = eta * FastMath.Identity(cosI);
+
+		if (sinT >= 1f)
 		{
-			CodeHelper.Swap(ref etaI, ref etaT);
-			cosI = -cosI;
+			//Total internal reflection
+			Utilities.Skip(out cosT);
+			return true;
 		}
 
-		//Apply Snell's law
-		float sinI = FastMath.Identity(cosI);
-		float sinT = etaI / etaT * sinI;
-		if (sinT >= 1f) return Float3.one; //Total internal reflection
-		float cosT = FastMath.Identity(sinT);
+		cosT = FastMath.Identity(sinT);
+		return false;
+	}
 
-		//Fresnel equation
-		float ti = etaT * cosI;
-		float it = etaI * cosT;
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	static float Apply(float cosI, float cosT, float etaI, float etaT)
+	{
+		float paraHead = etaT * cosI;
+		float paraTail = etaI * cosT;
 
-		float ii = etaI * cosI;
-		float tt = etaT * cosT;
+		float perpHead = etaI * cosI;
+		float perpTail = etaT * cosT;
 
-		float Rs = (ti - it) / (ti + it);
-		float Rp = (ii - tt) / (ii + tt);
+		float para = (paraHead - paraTail) / (paraHead + paraTail);
+		float perp = (perpHead - perpTail) / (perpHead + perpTail);
 
-		return (Float3)((Rs * Rs + Rp * Rp) / 2f);
+		return (para * para + perp * perp) / 2f;
+
+		// var m0 = Vector128.Create(etaT, etaI, etaI, etaT);
+		// var m1 = Vector128.Create(cosI, cosT, cosI, cosT);
+		// var m = Sse.Multiply(m0, m1);
+		//
+		// var s0 = Avx.Permute(m, 0b1111_0101);
+		// var s1 = Avx.Permute(m, 0b1010_0000);
+		// var s = Sse3.AddSubtract(s0, s1);
+		//
+		// var d0 = Avx.Permute(s, 0b1101_1101);
+		// var d1 = Avx.Permute(s, 0b1000_1000);
+		// var d = Sse.Divide(d0, d1);
+		//
+		// var dot = Sse.Multiply(Sse.Multiply(d, d), Vector128.Create(2f));
 	}
 }
 
 public readonly struct FresnelConductor
 {
-	public FresnelConductor(in Float3 newEtaI, in Float3 newEtaT, in Float3 newAbsorption)
+	public FresnelConductor(in Float3 etaAbove, in Float3 etaBelow, in Float3 absorption)
 	{
-		Float3 inverse = 1f / newEtaI;
+		Float3 etaIncidentR = 1f / etaAbove;
 
-		eta2 = newEtaT * inverse;
-		etaK2 = newAbsorption * inverse;
+		eta2 = etaBelow * etaIncidentR;
+		etaK2 = absorption * etaIncidentR;
 
 		eta2 *= eta2;
 		etaK2 *= etaK2;
 	}
 
-	readonly Float3 eta2;
-	readonly Float3 etaK2;
+	readonly Float3 eta2;  //(etaBelow   / etaAbove)^2
+	readonly Float3 etaK2; //(absorption / etaAbove)^2
 
 	public Float3 Evaluate(float cosI) //https://seblagarde.wordpress.com/2013/04/29/memo-on-fresnel-equations/
 	{
-		cosI = Math.Min(Math.Abs(cosI), 1f);
+		cosI = FastMath.Clamp11(cosI);
 
 		float cosI2 = cosI * cosI;
 		float sinI2 = 1f - cosI2;
 
-		Float3 gamma = eta2 - etaK2 - (Float3)sinI2;
-		Float3 sum = Sqrt(gamma * gamma + 4f * eta2 * etaK2);
+		Float3 term = eta2 - etaK2 - (Float3)sinI2;
+		Float3 a2b2 = Sqrt(term * term + 4f * eta2 * etaK2);
 
-		Float3 term0 = sum + (Float3)cosI2;
-		Float3 term1 = cosI * Scalars.Sqrt2 * Sqrt(sum + gamma);
+		//Parallel terms
+		Float3 a = Sqrt((a2b2 + term) / 2f);
+		Float3 paraHead = a2b2 + (Float3)cosI2;
+		Float3 paraTail = cosI * a * 2f;
 
-		Float3 term2 = cosI2 * sum + (Float3)(sinI2 * sinI2);
-		Float3 term3 = term1 * sinI2;
+		//Perpendicular terms
+		Float3 perpHead = cosI2 * a2b2 + (Float3)(sinI2 * sinI2);
+		Float3 perpTail = paraTail * sinI2;
 
-		Float3 Rs = (term0 - term1) / (term0 + term1);
-		Float3 Rp = (term2 - term3) / (term2 + term3);
+		//Combine the two terms
+		Float3 para = (paraHead - paraTail) / (paraHead + paraTail);
+		Float3 perp = (perpHead - perpTail) / (perpHead + perpTail);
 
-		return (Rs + Rs * Rp) / 2f;
+		return (para * perp + para) / 2f;
 	}
 
-	static Float3 Sqrt(in Float3 value) => new(MathF.Sqrt(value.x), MathF.Sqrt(value.y), MathF.Sqrt(value.z));
+	static Float3 Sqrt(in Float3 value) => new(FastMath.Sqrt0(value.x), FastMath.Sqrt0(value.y), FastMath.Sqrt0(value.z));
 }
--- EchoRenderer/src/Core/Rendering/Scattering/Specular.cs
+++ EchoRenderer/src/Core/Rendering/Scattering/Specular.cs
@@ -1,81 +1,95 @@
 ﻿using System;
 using CodeHelpers.Mathematics;
+using EchoRenderer.Common.Mathematics;
 using EchoRenderer.Core.Rendering.Distributions;
 
 namespace EchoRenderer.Core.Rendering.Scattering;
 
 public class SpecularReflection : BxDF
 {
-	public SpecularReflection() : base
-	(
-		FunctionType.reflective |
-		FunctionType.specular
-	) { }
+	public SpecularReflection() : base(FunctionType.specular | FunctionType.reflective) { }
+
+	public void Reset(in Float3 newReflectance)
+	{
+		reflectance = newReflectance;
+		mode = Mode.none;
+	}
 
 	public void Reset(in Float3 newReflectance, in FresnelDielectric newDielectric)
 	{
 		reflectance = newReflectance;
 		dielectric = newDielectric;
-		isDielectric = true;
+		mode = Mode.dielectric;
 	}
 
 	public void Reset(in Float3 newReflectance, in FresnelConductor newConductor)
 	{
 		reflectance = newReflectance;
 		conductor = newConductor;
-		isDielectric = false;
+		mode = Mode.conductor;
 	}
 
 	Float3 reflectance;
-	bool isDielectric;
+	Mode mode;
 
 	FresnelDielectric dielectric;
 	FresnelConductor conductor;
 
 	public override Float3 Evaluate(in Float3 outgoing, in Float3 incident) => Float3.zero;
+	public override float ProbabilityDensity(in Float3 outgoing, in Float3 incident) => 0f;
 
 	public override Float3 Sample(in Float3 outgoing, Sample2D sample, out Float3 incident, out float pdf)
 	{
-		incident = new Float3(-outgoing.x, -outgoing.y, outgoing.z);
-
 		pdf = 1f;
+
+		incident = new Float3(-outgoing.x, -outgoing.y, outgoing.z);
 		float cosI = CosineP(incident);
 
-		Float3 evaluation = isDielectric ? dielectric.Evaluate(cosI) : conductor.Evaluate(cosI);
-		return evaluation * reflectance / AbsoluteCosineP(incident);
+		Float3 evaluated = mode switch
+		{
+			Mode.dielectric => dielectric.Evaluate(cosI),
+			Mode.conductor => conductor.Evaluate(cosI),
+			_ => Float3.one
+		};
+
+		return evaluated * reflectance / FastMath.Abs(cosI);
 	}
 
-	public override float ProbabilityDensity(in Float3 outgoing, in Float3 incident) => 0f;
+	enum Mode
+	{
+		none,
+		dielectric,
+		conductor
+	}
 }
 
 public class SpecularTransmission : BxDF
 {
-	public SpecularTransmission() : base
-	(
-		FunctionType.transmissive |
-		FunctionType.specular
-	) { }
+	public SpecularTransmission() : base(FunctionType.specular | FunctionType.transmissive) { }
 
 	public void Reset(in Float3 newTransmittance, float newEtaAbove, float newEtaBelow)
 	{
 		transmittance = newTransmittance;
-		etaAbove = newEtaAbove;
-		etaBelow = newEtaBelow;
-
-		fresnel = new FresnelDielectric(etaAbove, etaBelow);
+		dielectric = new FresnelDielectric(newEtaAbove, newEtaBelow);
 	}
 
 	Float3 transmittance;
-	float etaAbove;
-	float etaBelow;
-
-	FresnelDielectric fresnel;
-
-	//TODO
+	FresnelDielectric dielectric;
 
 	public override Float3 Evaluate(in Float3 outgoing, in Float3 incident) => Float3.zero;
-
-	public override Float3 Sample(in Float3 outgoing, Sample2D sample, out Float3 incident, out float pdf) => throw new NotImplementedException();
-
 	public override float ProbabilityDensity(in Float3 outgoing, in Float3 incident) => 0f;
+
+	public override Float3 Sample(in Float3 outgoing, Sample2D sample, out Float3 incident, out float pdf)
+	{
+		pdf = 1f;
+
+		Float3 evaluated = Float3.one - dielectric.Evaluate(outgoing, out incident);
+		return evaluated * transmittance / Math.Abs(CosineP(incident));
+	}
+}
+
+public class SpecularFresnel : BxDF
+{
+	public SpecularFresnel() : base(FunctionType.specular | FunctionType.reflective | FunctionType.transmissive) { }
+	public override Float3 Evaluate(in Float3 outgoing, in Float3 incident) => throw new NotImplementedException();
 }
