1e2409caa4b59096a1b3d9ba37104d4c5f83c3ad
Gary Huang <henhaodegaigai@gmail.com>
2022-04-22 00:38:10 -0400

    Renamed methods

--- Echo/src/Core/Evaluation/Engines/ITilePattern.cs
+++ Echo/src/Core/Evaluation/Engines/ITilePattern.cs
@@ -9,7 +9,7 @@ namespace Echo.Core.Evaluation.Engines;
 public interface ITilePattern
 {
 	/// <summary>
-	/// Returns a sequence of tile positions from (0, 0) (Inclusive) to <paramref name="totalSize"/> (Exclusive) for rendering.
+	/// Returns a sequence of tile positions from (0, 0) (inclusive) to <paramref name="totalSize"/> (exclusive) for rendering.
 	/// NOTE: The returned positions are not scaled by tile size, meaning each positions should be consecutive to its neighbors.
 	/// </summary>
 	Int2[] GetPattern(Int2 totalSize);
@@ -35,10 +35,11 @@ public class ScrambledPattern : OrderedPattern
 public class SpiralPattern : ITilePattern
 {
 	/// <inheritdoc/>
-	public virtual Int2[] GetPattern(Int2 totalSize) => (from position in new EnumerableSpiral2D(totalSize.MaxComponent.CeiledDivide(2))
-														 let tile = position + totalSize / 2 - Int2.One
-														 where Int2.Zero <= tile && tile < totalSize
-														 select tile).ToArray();
+	public virtual Int2[] GetPattern(Int2 totalSize) =>
+		(from position in new EnumerableSpiral2D(totalSize.MaxComponent.CeiledDivide(2))
+		 let tile = position + totalSize / 2 - Int2.One
+		 where Int2.Zero <= tile && tile < totalSize
+		 select tile).ToArray();
 }
 
 public class CheckerboardPattern : SpiralPattern
