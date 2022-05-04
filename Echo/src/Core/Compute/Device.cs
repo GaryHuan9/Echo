a883ab83bd5c2cc9d986b0d95bf092b0bc3ed96e
Gary Huang <henhaodegaigai@gmail.com>
2022-05-03 22:05:36 -0400

    Basic compute systems added
    Former-commit-id: 683fe718907ee00ed5e95b5c6d743446ae7472ae

--- Echo/src/Core/Compute/Device.cs
+++ Echo/src/Core/Compute/Device.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Threading;
 using CodeHelpers.Diagnostics;
+using CodeHelpers.Threads;
 
 namespace Echo.Core.Compute;
 
-public sealed partial class Device : IDisposable
+public sealed class Device : IDisposable
 {
 	public Device(int population)
 	{
@@ -15,43 +16,108 @@ public sealed partial class Device : IDisposable
 	readonly Guid guid;
 	readonly Thread[] threads;
 
-	readonly object fieldsLocker = new();
-	readonly object manageLocker = new();
-	readonly object signalLocker = new();
+	readonly DualLock locker = new();
+	readonly Signaler signaler = new();
+
+	int idleCount;
 
 	public int Population => threads.Length;
-	public State Status { get; private set; }
 
-	public bool Paused { get; set; }
+	State _status;
+	Operation _operation;
+
+	public State Status
+	{
+		get
+		{
+			using (locker.FetchReadLock()) return _status;
+		}
+		private set
+		{
+			using (locker.FetchWriteLock())
+			{
+				if (_status == value) return;
+				AssertNotDisposed();
+				_status = value;
+			}
+
+			signaler.Signal();
+		}
+	}
+
+	public Operation Operation
+	{
+		get
+		{
+			using (locker.FetchReadLock()) return _status is State.Idle or State.Disposed ? null : _operation;
+		}
+		private set
+		{
+			using (locker.FetchWriteLock()) _operation = value;
+		}
+	}
 
 	public void Dispatch(Operation operation)
 	{
 		operation.Validate();
 
+		//Abandon previous operation if needed
+		using var _0 = locker.FetchUpgradeableReadLock();
+
 		Abandon();
 		AwaitIdle();
 
-		foreach (Thread thread in threads)
-		{
+		//Assign operation
+		using var _1 = locker.FetchWriteLock();
 
-		}
+		Operation = operation;
+		Status = State.Running;
+
+		//Launch threads
+		for (int i = 0; i < threads.Length; i++) LaunchThread(i);
 	}
 
-	public void Abandon() { }
+	public void Pause() { }
+
+	public void Resume() { }
+
+	public void Abandon()
+	{
+		if (Status == State.Idle) return;
+
+		using var _ = locker.FetchWriteLock();
+
+		if (Status == State.Idle) return;
+
+		Status = State.Abandoning;
+	}
 
 	public void Abort() { }
 
-	public void AwaitIdle() { }
+	public void AwaitIdle()
+	{
+		AwaitStatus(State.Idle);
+
+		Assert.IsNull(Operation);
+		Assert.IsTrue(Status == State.Idle);
+	}
 
 	public void Dispose()
 	{
+		using var _0 = locker.FetchUpgradeableReadLock();
+
 		Abort();
+		AwaitIdle();
+
+		using var _1 = locker.FetchWriteLock();
+
+		//TODO: dispose
 	}
 
-	void CreateThread(int index)
+	void LaunchThread(int index)
 	{
 		ref Thread thread = ref threads[index];
-		Assert.IsNull(thread);
+		if (thread != null) return;
 
 		thread = new Thread(Main)
 		{
@@ -64,25 +130,79 @@ public sealed partial class Device : IDisposable
 
 	void Main()
 	{
+		using var scheduler = new Scheduler();
+
 		while (true)
 		{
+			AwaitStatus(State.Running);
+
+			Operation operation = Operation;
+			if (operation == null) continue;
+
+			Interlocked.Increment(ref idleCount);
+
+			bool completed;
+
 			try
 			{
-
+				completed = !operation.Execute(scheduler);
 			}
 			catch (OperationAbortedException)
 			{
 				throw;
 			}
+
+			bool allIdle = Interlocked.Decrement(ref idleCount) == 0;
+
+			if (completed)
+			{
+				AssertNotDisposed();
+				CompleteOperation();
+			}
+			else if (allIdle)
+			{
+				using var _ = locker.FetchWriteLock();
+				Assert.AreEqual(InterlockedHelper.Read(ref idleCount), 0);
+
+				if (Status == State.Pausing) Status = State.Paused;
+				if (Status == State.Abandoning) Status = State.Idle;
+			}
 		}
 	}
+
+	void CompleteOperation()
+	{
+		if (HasNoOperation()) return;
+
+		using var _ = locker.FetchWriteLock();
+
+		if (HasNoOperation()) return;
+
+		Status = State.Idle;
+
+		bool HasNoOperation() => Status is not State.Running and not State.Pausing and not State.Abandoning and not State.Paused;
+	}
+
+	void AwaitStatus(State status)
+	{
+		while (true)
+		{
+			State current = Status;
+
+			if (current == status || current == State.Disposed) return;
+			lock (signaler) Monitor.Wait(signaler);
+		}
+	}
+
+	void AssertNotDisposed() => Assert.AreNotEqual(Status, State.Disposed);
 
 	public enum State
 	{
 		Idle,
 		Running,
+		Pausing,
+		Abandoning,
 		Paused,
 		Disposed
 	}
-
 }
