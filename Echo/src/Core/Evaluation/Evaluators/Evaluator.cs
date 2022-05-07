36193f18331bf6e305a30f80f5bb33df6f979bf6
Gary Huang <henhaodegaigai@gmail.com>
2022-05-06 23:38:04 -0400

    Moved the scene out of the Evaluator

--- Echo/src/Core/Evaluation/Evaluators/Evaluator.cs
+++ Echo/src/Core/Evaluation/Evaluators/Evaluator.cs
@@ -1,12 +1,13 @@
 ﻿using CodeHelpers;
 using CodeHelpers.Packed;
 using Echo.Common.Mathematics.Primitives;
-using Echo.Common.Mathematics.Randomization;
 using Echo.Common.Memory;
+using Echo.Core.Evaluation.Distributions;
 using Echo.Core.Evaluation.Distributions.Continuous;
 using Echo.Core.Evaluation.Operations;
 using Echo.Core.Scenic;
 using Echo.Core.Scenic.Preparation;
+using Echo.Core.Textures;
 using Echo.Core.Textures.Colors;
 
 namespace Echo.Core.Evaluation.Evaluators;
@@ -18,20 +19,12 @@ public abstract record Evaluator
 	protected Evaluator(Evaluator source)
 	{
 		allocator = source.allocator with { };
-		Scene = source.Scene;
 		Distribution = source.Distribution with { };
 	}
 
 	protected readonly Allocator allocator = new();
 
-	readonly NotNull<PreparedScene> _scene = PreparedScene.empty;
 	readonly NotNull<ContinuousDistribution> _distribution = new StratifiedDistribution();
-
-	public PreparedScene Scene
-	{
-		get => _scene;
-		init => _scene = value;
-	}
 
 	public ContinuousDistribution Distribution
 	{
@@ -42,16 +35,17 @@ public abstract record Evaluator
 	/// <summary>
 	/// Evaluates <see cref="Scene"/> based on <see cref="Distribution"/> using many samples.
 	/// </summary>
-	/// <param name="spawner">The <see cref="RaySpawner"/> we use to create new rays for evaluation.</param>
+	/// <param name="scene"></param>
+	/// <param name="region"></param>
 	/// <param name="accumulator">Samples of the evaluation are added to this <see cref="Accumulator"/>.</param>
 	/// <returns>The number of invalid samples that are rejected.</returns>
 	/// <remarks>The exact number of samples that are used to evaluates <see cref="Scene"/> is <see cref="ContinuousDistribution.Extend"/>.</remarks>
-	public abstract uint Evaluate(RaySpawner spawner, ref Accumulator accumulator);
+	public abstract uint Evaluate(PreparedScene scene, TextureRegion region, ref Accumulator accumulator);
 }
 
 public abstract record Evaluator<T> : Evaluator where T : IColor<T>
 {
-	public sealed override uint Evaluate(RaySpawner spawner, ref Accumulator accumulator)
+	public sealed override uint Evaluate(PreparedScene scene, TextureRegion region, ref Accumulator accumulator)
 	{
 		uint rejection = 0;
 
@@ -59,7 +53,8 @@ public abstract record Evaluator<T> : Evaluator where T : IColor<T>
 		{
 			Distribution.BeginSession();
 
-			Ray ray = spawner.Spawn(Scene.camera, Distribution);
+			Ray ray = scene.camera.SpawnRay(new CameraSample(Distribution), region);
+
 			Float4 sampled = Evaluate(ray).ToFloat4();
 			if (!accumulator.Add(sampled)) ++rejection;
 
@@ -70,10 +65,11 @@ public abstract record Evaluator<T> : Evaluator where T : IColor<T>
 	}
 
 	/// <summary>
-	/// Evaluates <see cref="Evaluator.Scene"/> through <paramref name="ray"/>.
+	/// Evaluates a <see cref="PreparedScene"/> through a <see cref="Ray"/>.
 	/// </summary>
+	/// <param name="scene">The <see cref="PreparedScene"/> to evaluate.</param>
 	/// <param name="ray">The <see cref="Ray"/> that we should start evaluation on.</param>
 	/// <returns>The evaluated value of type <typeparamref name="T"/>.</returns>
 	/// <remarks>The implementation do not need to invoke <see cref="Allocator.Release"/> before this method returns.</remarks>
-	protected abstract T Evaluate(in Ray ray);
+	protected abstract T Evaluate(PreparedScene scene, in Ray ray);
 }
