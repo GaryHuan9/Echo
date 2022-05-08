c1a741584db3faea4c55159ebe4151416d7d4865
Gary Huang <henhaodegaigai@gmail.com>
2022-05-08 15:12:49 -0400

    Basic Terminal fundation working
    Former-commit-id: 1d34502d0f0e1ce6891b519dd5933244173f1e4d

---
+++ Echo.Terminal/Areas/AreaUI.cs
@@ -0,0 +1,28 @@
+﻿using CodeHelpers.Diagnostics;
+
+namespace Echo.Terminal.Areas;
+
+public abstract class AreaUI
+{
+	Domain _domain;
+
+	public Domain Domain
+	{
+		get => _domain;
+		set
+		{
+			if (_domain == value) return;
+
+			var old = _domain;
+			_domain = value;
+			OnResize(old);
+		}
+	}
+
+	public abstract void Update();
+
+	protected virtual void OnResize(Domain previous)
+	{
+		Assert.AreNotEqual(previous, Domain);
+	}
+}
