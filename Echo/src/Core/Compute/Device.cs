f065cbd0ba780ffb14d223e4faa8934df9cf6f4e
Gary Huang <henhaodegaigai@gmail.com>
2022-05-07 21:11:41 -0400

    New Device working

--- Echo/src/Core/Compute/Device.cs
+++ Echo/src/Core/Compute/Device.cs
@@ -1,216 +1,99 @@
 ﻿using System;
-using System.Threading;
 using CodeHelpers.Diagnostics;
-using CodeHelpers.Threads;
+using Echo.Common;
 
 namespace Echo.Core.Compute;
 
-public sealed partial class Device : IDisposable
+public sealed class Device : IDisposable
 {
 	public Device() : this(Environment.ProcessorCount) { }
 
 	public Device(int population)
 	{
-		guid = Guid.NewGuid();
-		threads = new Thread[population];
+		workers = new Worker[population];
+
+		for (int i = 0; i < workers.Length; i++)
+		{
+			ref Worker worker = ref workers[i];
+			worker = new Worker((uint)i);
+
+			worker.OnRunEvent += OnWorkerRun;
+			worker.OnIdleEvent += OnWorkerIdle;
+		}
 	}
 
-	readonly Guid guid;
-	readonly Thread[] threads;
-
-	readonly DualLock locker = new();
-	readonly Signaler signaler = new();
+	readonly Worker[] workers;
 
 	int runningCount;
 
-	public int Population => threads.Length;
+	readonly Locker manageLocker = new();
+	readonly Locker signalLocker = new();
 
-	State _status;
-	Operation _operation;
-
-	public State Status
+	public bool IsIdle
 	{
 		get
 		{
-			using (locker.FetchReadLock()) return _status;
-		}
-		private set
-		{
-			using (locker.FetchWriteLock())
-			{
-				if (_status == value) return;
-				AssertNotDisposed();
-				_status = value;
-			}
-
-			signaler.Signal();
+			using var _ = signalLocker.Fetch();
+			Assert.IsFalse(runningCount < 0);
+			return runningCount == 0;
 		}
 	}
 
-	public Operation Operation
+	public void AwaitIdle()
 	{
-		get
-		{
-			using (locker.FetchReadLock()) return _status is State.Idle or State.Disposed ? null : _operation;
-		}
-		private set
-		{
-			using (locker.FetchWriteLock()) _operation = value;
-		}
+		using var _ = signalLocker.Fetch();
+		while (runningCount > 0) signalLocker.Wait();
+		Assert.IsFalse(runningCount < 0);
 	}
 
 	public void Dispatch(Operation operation)
 	{
-		operation.Prepare(Population);
+		operation.Prepare(workers.Length);
+		using var _ = manageLocker.Fetch();
 
-		//Abandon previous operation if needed
-		using var _0 = locker.FetchUpgradeableReadLock();
-
-		Abandon();
-		AwaitIdle();
-
-		//Assign operation
-		using var _1 = locker.FetchWriteLock();
-
-		Operation = operation;
-		Status = State.Running;
-
-		//Launch threads
-		for (uint i = 0; i < threads.Length; i++)
+		if (!IsIdle)
 		{
-			ref Thread thread = ref threads[i];
-			if (thread != null) continue;
-
-			thread = new Thread(Main)
-			{
-				IsBackground = true, Priority = ThreadPriority.Normal,
-				Name = $"{nameof(Device)} {guid} {nameof(Thread)} {i}"
-			};
-
-			thread.Start(i);
+			Abort();
+			AwaitIdle();
 		}
+
+		foreach (var worker in workers) worker.Dispatch(operation);
 	}
 
-	public void Pause() { }
-
-	public void Resume() { }
-
-	public void Abandon()
+	public void Pause()
 	{
-		if (Status == State.Idle) return;
-
-		using var _ = locker.FetchWriteLock();
-
-		if (Status == State.Idle) return;
-
-		Status = State.Abandoning;
+		using var _ = manageLocker.Fetch();
+		foreach (var worker in workers) worker.Pause();
 	}
 
-	public void Abort() { }
-
-	public void AwaitIdle()
+	public void Resume()
 	{
-		AwaitStatus(State.Idle);
-
-		Assert.IsNull(Operation);
-		Assert.IsTrue(Status == State.Idle);
+		using var _ = manageLocker.Fetch();
+		foreach (var worker in workers) worker.Resume();
 	}
 
-	public void Dispose()
+	public void Abort()
 	{
-		using var _0 = locker.FetchUpgradeableReadLock();
-
-		Abort();
-		AwaitIdle();
-
-		using var _1 = locker.FetchWriteLock();
-
-		//TODO: dispose
+		using var _ = manageLocker.Fetch();
+		foreach (var worker in workers) worker.Abort();
 	}
 
-	void Main(object id)
+	void IDisposable.Dispose() { }
+
+	void OnWorkerRun(Worker worker)
 	{
-		using var scheduler = new Scheduler((uint)id);
-
-		while (true)
-		{
-			AwaitStatus(State.Running);
-
-			Operation operation = Operation;
-			if (operation == null) continue;
-
-			bool running;
-
-			Interlocked.Increment(ref runningCount);
-
-			do
-			{
-				try
-				{
-					running = operation.Execute(scheduler);
-				}
-				catch (OperationAbortedException)
-				{
-					throw;
-				}
-			}
-			while (running);
-
-			if (Interlocked.Decrement(ref runningCount) == 0)
-			{
-				CompleteOperation();
-			}
-			else AwaitStatus(State.Idle);
-
-			// if (completed)
-			// {
-			// 	AssertNotDisposed();
-			// 	CompleteOperation();
-			// }
-			// else if (allIdle)
-			// {
-			// 	using var _ = locker.FetchWriteLock();
-			// 	Assert.AreEqual(InterlockedHelper.Read(ref runningCount), 0);
-			//
-			// 	if (Status == State.Pausing) Status = State.Paused;
-			// 	if (Status == State.Abandoning) Status = State.Idle;
-			// }
-		}
+		using var _ = signalLocker.Fetch();
+		Assert.IsFalse(runningCount >= workers.Length);
+		++runningCount;
 	}
 
-	void CompleteOperation()
+	void OnWorkerIdle(Worker worker)
 	{
-		if (HasNoOperation()) return;
+		using var _ = signalLocker.Fetch();
+		Assert.IsFalse(runningCount <= 0);
+		--runningCount;
 
-		using var _ = locker.FetchWriteLock();
-
-		if (HasNoOperation()) return;
-
-		Status = State.Idle;
-
-		bool HasNoOperation() => Status is not State.Running and not State.Pausing and not State.Abandoning and not State.Paused;
-	}
-
-	void AwaitStatus(State status)
-	{
-		while (true)
-		{
-			State current = Status;
-
-			if (current == status || current == State.Disposed) return;
-			signaler.Wait();
-		}
-	}
-
-	void AssertNotDisposed() => Assert.AreNotEqual(Status, State.Disposed);
-
-	public enum State
-	{
-		Idle,
-		Running,
-		Pausing,
-		Abandoning,
-		Paused,
-		Disposed
+		//Signal if all workers are idle
+		if (runningCount == 0) signalLocker.Signal();
 	}
 }
---
+++ Echo/src/Core/Compute/Worker.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Numerics;
+using System.Threading;
+using CodeHelpers.Diagnostics;
+using Echo.Common;
+
+namespace Echo.Core.Compute;
+
+public class Worker : IScheduler
+{
+	public Worker(uint id)
+	{
+		Id = id;
+	}
+
+	Operation nextOperation;
+	Thread thread;
+
+	public event Action<Worker> OnRunEvent;
+	public event Action<Worker> OnIdleEvent;
+
+	/// <inheritdoc/>
+	public uint Id { get; }
+
+	uint _status = (uint)State.Idle;
+
+	public State Status
+	{
+		get => (State)Volatile.Read(ref _status);
+		private set
+		{
+			using var _ = manageLocker.Fetch();
+
+			State old = Status;
+			if (old == value) return;
+			uint integer = (uint)value;
+
+			Assert.AreNotEqual(old, State.Disposed);
+			Assert.AreEqual(BitOperations.PopCount(integer), 1);
+
+			Volatile.Write(ref _status, integer);
+			signalLocker.Signal();
+
+			bool wasIdle = old == State.Idle;
+			bool isIdle = value == State.Idle;
+			if (wasIdle != isIdle) (isIdle ? OnIdleEvent : OnRunEvent)?.Invoke(this);
+		}
+	}
+
+	readonly Locker manageLocker = new();
+	readonly Locker signalLocker = new();
+
+	public void Dispatch(Operation operation)
+	{
+		lock (manageLocker)
+		{
+			if (Status != State.Idle) throw new InvalidOperationException();
+
+			//Launch thread if needed
+			if (thread == null)
+			{
+				thread = new Thread(Main)
+				{
+					IsBackground = true, Priority = ThreadPriority.Normal,
+					Name = $"{nameof(Worker)} Thread {Guid.NewGuid():N}"
+				};
+
+				thread.Start();
+			}
+
+			//Assign operation
+			Volatile.Write(ref nextOperation, operation);
+			Status = State.Running;
+		}
+	}
+
+	public void Pause()
+	{
+		lock (manageLocker)
+		{
+			switch (Status)
+			{
+				case State.Running: break;
+				case State.Idle:
+				case State.Pausing:
+				case State.Aborting:
+				case State.Awaiting: return;
+				case State.Disposed: throw new InvalidOperationException();
+				default:             throw new ArgumentOutOfRangeException();
+			}
+
+			Status = State.Pausing;
+		}
+	}
+
+	public void Resume()
+	{
+		lock (manageLocker)
+		{
+			switch (Status)
+			{
+				case State.Pausing:
+				case State.Awaiting: break;
+				case State.Idle:
+				case State.Running:
+				case State.Aborting: return;
+				case State.Disposed: throw new InvalidOperationException();
+				default:             throw new ArgumentOutOfRangeException();
+			}
+
+			Status = State.Running;
+		}
+	}
+
+	public void Abort()
+	{
+		lock (manageLocker)
+		{
+			switch (Status)
+			{
+				case State.Running:
+				case State.Pausing:
+				case State.Awaiting: break;
+				case State.Idle:
+				case State.Aborting: return;
+				case State.Disposed: throw new InvalidOperationException();
+				default:             throw new ArgumentOutOfRangeException();
+			}
+
+			Status = State.Aborting;
+		}
+	}
+
+	/// <inheritdoc/>
+	void IScheduler.CheckSchedule()
+	{
+		CheckForAbortion();
+		if (Status != State.Pausing) return;
+
+		lock (manageLocker)
+		{
+			if (Status != State.Pausing) return;
+			Status = State.Awaiting;
+		}
+
+		AwaitStatus(State.Running | State.Aborting);
+		CheckForAbortion();
+
+		void CheckForAbortion()
+		{
+			if (Status != State.Aborting) return;
+			throw new OperationAbortedException();
+		}
+	}
+
+	void Main()
+	{
+		while (true)
+		{
+			AwaitStatus(State.Running);
+
+			var operation = Volatile.Read(ref nextOperation);
+			if (operation == null) continue;
+
+			bool running;
+
+			do
+			{
+				try
+				{
+					running = operation.Execute(this);
+				}
+				catch (OperationAbortedException)
+				{
+					break;
+				}
+			}
+			while (running);
+
+			lock (manageLocker) Status = State.Idle;
+		}
+	}
+
+	void AwaitStatus(State status)
+	{
+		while ((Status | status) != status)
+		{
+			lock (signalLocker) Monitor.Wait(signalLocker);
+		}
+	}
+
+	[Flags]
+	public enum State : uint
+	{
+		Idle = 1 << 0,
+		Running = 1 << 1,
+		Pausing = 1 << 2,
+		Aborting = 1 << 3,
+		Awaiting = 1 << 4,
+		Disposed = 1 << 5
+	}
+}
