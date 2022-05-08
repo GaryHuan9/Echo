8d339c90acecd44e334ac4ce9890cd67ecb02c53
Gary Huang <henhaodegaigai@gmail.com>
2022-05-08 19:09:05 -0400

    Organized folder

---
+++ Echo.Terminal/src/Interface/ControlUI.cs
@@ -0,0 +1,12 @@
+﻿using Echo.Terminal.Core;
+
+namespace Echo.Terminal.Interface;
+
+public class ControlUI : AreaUI
+{
+	public override void Update()
+	{
+		Domain.Fill();
+		Domain.WriteLine(0, ">> ");
+	}
+}
