9d7efffcb8d60816992242b786e671d934957f65
Gary Huang <henhaodegaigai@gmail.com>
2022-05-10 22:59:31 -0400

    All four WrapOptions working

--- Echo.Terminal/src/Application/CommandTI.cs
+++ Echo.Terminal/src/Application/CommandTI.cs
@@ -6,17 +6,27 @@ namespace Echo.Terminal.Application;
 
 public class CommandTI : AreaTI
 {
-	public CommandTI() => InvertY = true;
-
 	protected override void Draw(in Domain.Drawer drawer)
 	{
 		drawer.FillAll();
 
 		Int2 position = Int2.Zero;
 
-		const string Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vulputate quam in condimentum ultrices. Aenean quis tortor ac tellus scelerisque volutpat nec scelerisque ipsum. Donec at eleifend ipsum, sit amet blandit nisi. Aenean rhoncus fringilla sollicitudin. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas faucibus congue vehicula. Curabitur molestie malesuada risus. Donec maximus dui urna, eget aliquam felis fringilla at. Praesent varius rutrum magna non sollicitudin. Nullam in purus sit amet tellus elementum imperdiet at sit amet diam. Vestibulum consectetur lacus magna, sed dignissim justo suscipit eu. Maecenas sed odio nulla. Donec tristique elit et metus iaculis pretium.";
+		const string Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+							"Morbi vulputate quam in condimentum ultrices. " +
+							"Donec quis tortor ac tellus scelerisque volutpat nec scelerisque ipsum. " +
+							"Aenean rhoncus fringilla sollicitudin. ";
 
-		position = drawer.WriteLine(position, Text, new TextOptions { WrapOptions = WrapOptions.LineBreak, Truncate = false });
-		position = drawer.WriteLine(position, Text, new TextOptions { WrapOptions = WrapOptions.NoWrap, Truncate = false });
+		position = drawer.WriteLine(position, Text, new TextOptions { WrapOptions = WrapOptions.NoWrap });
+		position = drawer.FillLine(position);
+
+		position = drawer.WriteLine(position, Text, new TextOptions { WrapOptions = WrapOptions.LineBreak });
+		position = drawer.FillLine(position);
+
+		position = drawer.WriteLine(position, Text, new TextOptions { WrapOptions = WrapOptions.WordBreak });
+		position = drawer.FillLine(position);
+
+		position = drawer.WriteLine(position, Text, new TextOptions { WrapOptions = WrapOptions.Justified });
+		position = drawer.FillLine(position);
 	}
 }
