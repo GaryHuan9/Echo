89e35b0ea718b1440ff5571cfeab8deebf95fd6c
Gary Huang <henhaodegaigai@gmail.com>
2022-05-18 22:53:54 -0400

    Benchmarked memory copy & added ArrayGrid pinning

--- Echo/src/Core/Textures/Grid/RenderBuffer.cs
+++ Echo/src/Core/Textures/Grid/RenderBuffer.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
 using CodeHelpers;
 using CodeHelpers.Collections;
 using CodeHelpers.Packed;
@@ -25,7 +21,7 @@ public class RenderBuffer : ArrayGrid<RGB128>
 	/// Tries to access the <see cref="TextureGrid{T}"/> buffer layer named <paramref name="label"/> for type
 	/// <typeparamref name="T"/> and outputs it to <paramref name="layer"/>. Returns whether the operation succeeded.
 	/// </summary>
-	public bool TryGetLayer<T>(string label, out TextureGrid<T> layer) where T : IColor<T>
+	public bool TryGetLayer<T>(string label, out TextureGrid<T> layer) where T : unmanaged, IColor<T>
 	{
 		if (layers.TryGetValue(label, out Texture candidate))
 		{
@@ -40,7 +36,7 @@ public class RenderBuffer : ArrayGrid<RGB128>
 	/// <summary>
 	/// Creates a new layer named <paramref name="label"/>.
 	/// </summary>
-	public void CreateLayer<T>(string label) where T : IColor<T>
+	public void CreateLayer<T>(string label) where T : unmanaged, IColor<T>
 	{
 		if (!layers.ContainsKey(label)) layers.Add(label, new ArrayGrid<T>(size));
 		else throw ExceptionHelper.Invalid(nameof(label), label, InvalidType.foundDuplicate);
@@ -52,21 +48,21 @@ public class RenderBuffer : ArrayGrid<RGB128>
 
 		if (texture is RenderBuffer buffer)
 		{
-			Parallel.ForEach(layers, pair =>
+			foreach (var pair in layers)
 			{
-				if (pair.Value == this) return;
+				if (pair.Value == this) continue;
 
 				var source = buffer.layers.TryGetValue(pair.Key);
 				if (source != null) pair.Value.CopyFrom(source);
-			});
+			}
 		}
 		else
 		{
-			Parallel.ForEach(layers, pair =>
+			foreach (var pair in layers)
 			{
-				if (pair.Value == this) return;
+				if (pair.Value == this) continue;
 				pair.Value.CopyFrom(texture);
-			});
+			}
 		}
 	}
 
@@ -74,51 +70,4 @@ public class RenderBuffer : ArrayGrid<RGB128>
 	/// Completely empties the content of all of the layers and buffers in this <see cref="RenderBuffer"/>.
 	/// </summary>
 	public virtual void Clear() => CopyFrom(black);
-
-	/// <summary>
-	/// Pins this <see cref="RenderBuffer"/> for various unmanaged access or pointer shenanigans.
-	/// NOTE: Remember to dispose <see cref="Pin"/> after use to unpin the buffers and handles.
-	/// </summary>
-	public Pin CreatePin() => new(this);
-
-	public sealed class Pin : IDisposable
-	{
-		/// <summary>
-		/// Pins data inside <paramref name="buffer"/> for unmanaged use and holds pins until disposed.
-		/// </summary>
-		public Pin(RenderBuffer buffer)
-		{
-			layers = buffer.layers.ToDictionary(pair => pair.Key, pair =>
-			{
-				object array = arrayFieldInfo.GetValue(pair.Value);
-				var handle = GCHandle.Alloc(array, GCHandleType.Pinned);
-				return new Bundle(handle.AddrOfPinnedObject(), handle);
-			}, StringComparer.InvariantCultureIgnoreCase);
-		}
-
-		static Pin() => arrayFieldInfo = typeof(ArrayGrid<>).GetField(nameof(pixels), BindingFlags.Public | BindingFlags.NonPublic);
-
-		readonly Dictionary<string, Bundle> layers;
-		static readonly FieldInfo arrayFieldInfo;
-
-		public IntPtr this[string label] => layers[label].pointer;
-
-		public void Dispose()
-		{
-			foreach (var pair in layers) pair.Value.handle.Free();
-			layers.Clear();
-		}
-
-		struct Bundle
-		{
-			public Bundle(IntPtr pointer, GCHandle handle)
-			{
-				this.pointer = pointer;
-				this.handle = handle;
-			}
-
-			public readonly IntPtr pointer;
-			public readonly GCHandle handle;
-		}
-	}
 }
