70b5f1df60af98953e0dbf0ce24157e905360012
Gary Huang <henhaodegaigai@gmail.com>
2022-05-19 21:53:19 -0400

    Fixed RenderBuffer writing
    Former-commit-id: 002f98a2402ba6a71a4e3d923ec236ecf8f961c5

--- Echo/src/Core/Textures/Grid/RenderBuffer.cs
+++ Echo/src/Core/Textures/Grid/RenderBuffer.cs
@@ -3,42 +3,67 @@ using System.Collections.Generic;
 using CodeHelpers;
 using CodeHelpers.Collections;
 using CodeHelpers.Packed;
+using Echo.Core.Evaluation.Operations;
 using Echo.Core.Textures.Colors;
 
 namespace Echo.Core.Textures.Grid;
 
 /// <summary>
-/// A <see cref="ArrayGrid{T}"/> of type <see cref="RGB128"/> primarily used as a rendering destination.
-/// Allows for optional auxiliary layers for data such as albedo or normal support later reconstruction.
+/// A collection of layers of <see cref="ArrayGrid{T}"/> primarily used as a rendering destination.
+/// Allows for optional auxiliary evaluation layers (such as normal) to support later reconstruction.
 /// </summary>
 public class RenderBuffer : ArrayGrid<RGB128>
 {
-	public RenderBuffer(Int2 size) : base(size) => layers.Add("main", this);
+	public RenderBuffer(Int2 size) : base(size) => AddLayer("main", this);
 
-	readonly Dictionary<string, Texture> layers = new(StringComparer.InvariantCultureIgnoreCase);
+	readonly Dictionary<string, Layer> layers = new(StringComparer.InvariantCultureIgnoreCase);
 
-	/// <summary>
-	/// Tries to access the <see cref="TextureGrid{T}"/> buffer layer named <paramref name="label"/> for type
-	/// <typeparamref name="T"/> and outputs it to <paramref name="layer"/>. Returns whether the operation succeeded.
-	/// </summary>
-	public bool TryGetLayer<T>(string label, out TextureGrid<T> layer) where T : unmanaged, IColor<T>
+	/// <inheritdoc cref="TryGetTexture"/>
+	public bool TryGetTexture<T>(string label, out TextureGrid<T> texture) where T : unmanaged, IColor<T>
 	{
-		if (layers.TryGetValue(label, out Texture candidate))
+		if (TryGetTexture(label, out Texture candidate))
 		{
-			layer = candidate as TextureGrid<T>;
-			return layer != null;
+			texture = candidate as TextureGrid<T>;
+			return texture != null;
 		}
 
-		layer = null;
+		texture = null;
 		return false;
 	}
 
 	/// <summary>
-	/// Creates a new layer named <paramref name="label"/>.
+	/// Tries to get a layer <see cref="Texture"/> from this <see cref="RenderBuffer"/>.
 	/// </summary>
+	/// <param name="label">The label of the layer to find.</param>
+	/// <param name="texture">Outputs the texture if found.</param>
+	/// <returns>Whether a matching texture is found.</returns>
+	public bool TryGetTexture(string label, out Texture texture)
+	{
+		texture = layers.TryGetValue(label).texture;
+		return texture != null;
+	}
+
+	/// <summary>
+	/// Tries to get a layer <see cref="Writer"/> from this <see cref="RenderBuffer"/>.
+	/// </summary>
+	/// <param name="label">The label of the layer to find.</param>
+	/// <param name="writer">Outputs the <see cref="Writer"/> if found.</param>
+	/// <returns>Whether a matching <see cref="Writer"/> is found.</returns>
+	/// <seealso cref="Writer"/>
+	public bool TryGetWriter(string label, out Writer writer)
+	{
+		writer = layers.TryGetValue(label).writer;
+		return writer != null;
+	}
+
+	/// <summary>
+	/// Creates a new layer.
+	/// </summary>
+	/// <param name="label">The <see cref="string"/> to name this new
+	/// layer. This <see cref="string"/> is case insensitive.</param>
 	public void CreateLayer<T>(string label) where T : unmanaged, IColor<T>
 	{
-		if (!layers.ContainsKey(label)) layers.Add(label, new ArrayGrid<T>(size));
+		if (!layers.ContainsKey(label)) AddLayer(label, new ArrayGrid<T>(size));
 		else throw ExceptionHelper.Invalid(nameof(label), label, InvalidType.foundDuplicate);
 	}
 
@@ -50,18 +75,18 @@ public class RenderBuffer : ArrayGrid<RGB128>
 		{
 			foreach (var pair in layers)
 			{
-				if (pair.Value == this) continue;
+				if (pair.Value.texture == this) continue;
 
-				var source = buffer.layers.TryGetValue(pair.Key);
-				if (source != null) pair.Value.CopyFrom(source);
+				var source = buffer.layers.TryGetValue(pair.Key).texture;
+				if (source != null) pair.Value.texture.CopyFrom(source);
 			}
 		}
 		else
 		{
 			foreach (var pair in layers)
 			{
-				if (pair.Value == this) continue;
-				pair.Value.CopyFrom(texture);
+				if (pair.Value.texture == this) continue;
+				pair.Value.texture.CopyFrom(texture);
 			}
 		}
 	}
@@ -70,4 +95,24 @@ public class RenderBuffer : ArrayGrid<RGB128>
 	/// Completely empties the content of all of the layers and buffers in this <see cref="RenderBuffer"/>.
 	/// </summary>
 	public virtual void Clear() => CopyFrom(black);
+
+	void AddLayer<T>(string label, TextureGrid<T> texture) where T : unmanaged, IColor<T>
+	{
+		layers.Add(label, new Layer(texture, Write));
+
+		void Write(Int2 position, in Accumulator accumulator) => texture[position] = default(T).FromFloat4(accumulator.Value);
+	}
+
+	/// <summary>
+	/// Delegate used to write to a specific layer in a <see cref="RenderBuffer"/>.
+	/// </summary>
+	/// <param name="position">The position to write. This is the same as the <see cref="TextureGrid{T}.Item(Int2)"/> indexer.</param>
+	/// <param name="accumulator">The value to write is exacted from this <see cref="Accumulator"/>.</param>
+	public delegate void Writer(Int2 position, in Accumulator accumulator);
+
+	readonly record struct Layer(Texture texture, Writer writer)
+	{
+		public readonly Texture texture = texture;
+		public readonly Writer writer = writer;
+	}
 }
