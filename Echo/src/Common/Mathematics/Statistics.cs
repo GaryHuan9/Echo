3322a371797d47da445a0d3b13f30083cff70b59
Gary Huang <henhaodegaigai@gmail.com>
2022-05-22 23:11:18 -0400

    Source generator working!!!

---
+++ Echo/src/Common/Mathematics/Statistics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Echo.Common.Mathematics;
+
+public partial struct Statistics
+{
+	public partial void Report(string label);
+
+	// public partial void Report(string label) { } 
+
+	public void ReportConst()
+	{
+		this.Report("rider");
+	}
+}
