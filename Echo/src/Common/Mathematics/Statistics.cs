a454a58b70c4a1ba59ea7342647fc33f71225f18
Gary Huang <henhaodegaigai@gmail.com>
2022-05-23 23:16:25 -0400

    Added Sum method to generated Statistics

--- Echo/src/Common/Mathematics/Statistics.cs
+++ Echo/src/Common/Mathematics/Statistics.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-
-namespace Echo.Common.Mathematics;
+﻿namespace Echo.Common.Mathematics;
 
 public partial struct Statistics
 {
 	public partial void Report(string label);
 
-	// public partial void Report(string label) { } 
-
 	public void ReportConst()
 	{
 		this.Report("rider");
 	}
+
+	public static unsafe partial Statistics Sum(Statistics* source, int length);
 }
