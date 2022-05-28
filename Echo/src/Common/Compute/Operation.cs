b786efadd00f34e0dcfcd5e7baa59d5a1883344a
Gary Huang <henhaodegaigai@gmail.com>
2022-05-28 19:40:33 -0400

    Added generic variant of Operation

--- Echo/src/Common/Compute/Operation.cs
+++ Echo/src/Common/Compute/Operation.cs
@@ -1,14 +1,25 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
+using Echo.Common.Memory;
 
 namespace Echo.Common.Compute;
 
 /// <summary>
 /// A task with individual steps to be executed both sequentially and concurrently.
 /// </summary>
-public abstract class Operation
+public abstract class Operation : IDisposable
 {
 	ulong nextProcedure;
-	ulong procedureCount;
+
+	/// <summary>
+	/// The number of steps that is either currently executing or has already been executed.
+	/// </summary>
+	public ulong StartedProcedureCount => Math.Min(TotalProcedureCount, nextProcedure);
+
+	/// <summary>
+	/// The total number of steps that this operation has.
+	/// </summary>
+	public ulong TotalProcedureCount { get; private set; }
 
 	/// <summary>
 	/// Prepares this <see cref="Operation"/> for execution.
@@ -16,10 +27,10 @@ public abstract class Operation
 	/// <param name="population">The maximum number of concurrent <see cref="IScheduler"/> executing. Note that
 	/// all <see cref="IScheduler.Id"/> will be between zero (inclusive) and this value (exclusive).</param>
 	/// <remarks>This method is invoked once before all execution begins</remarks>
-	public void Prepare(int population)
+	public virtual void Prepare(int population)
 	{
-		Interlocked.Exchange(ref nextProcedure, unchecked((ulong)0 - 1));
-		procedureCount = WarmUp(population);
+		Interlocked.Exchange(ref nextProcedure, 0);
+		TotalProcedureCount = WarmUp(population);
 	}
 
 	/// <summary>
@@ -29,11 +40,17 @@ public abstract class Operation
 	/// <returns>Whether this execution performed any work.</returns>
 	public bool Execute(IScheduler scheduler)
 	{
-		ulong procedure = Interlocked.Increment(ref nextProcedure);
-		if (procedure >= procedureCount) return false;
+		ulong procedure = Interlocked.Increment(ref nextProcedure) - 1;
+		if (procedure >= TotalProcedureCount) return false;
 
 		Execute(procedure, scheduler);
 		return true;
+	}
+
+	public void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
 	}
 
 	/// <inheritdoc cref="Prepare"/>
@@ -48,4 +65,57 @@ public abstract class Operation
 	/// this <see cref="Operation"/> gets completed.</param>
 	/// <param name="scheduler">The to <see cref="IScheduler"/> use.</param>
 	protected abstract void Execute(ulong procedure, IScheduler scheduler);
+
+	/// <summary>
+	/// Releases the resources owned by this <see cref="Operation"/>.
+	/// </summary>
+	/// <param name="disposing">If true, this is invoked by the <see cref="IDisposable.Dispose"/> method, otherwise it is invoked by the finalizer.</param>
+	protected virtual void Dispose(bool disposing) { }
+}
+
+/// <summary>
+/// An <see cref="Operation"/> that supports event recording through <see cref="IStatistics{T}"/>.
+/// </summary>
+/// <typeparam name="T">The type of <see cref="IStatistics{T}"/> to use.</typeparam>
+public abstract class Operation<T> : Operation where T : unmanaged, IStatistics<T>
+{
+	AlignedArray<T> array;
+	int statisticsCount;
+
+	/// <summary>
+	/// Retrieves the current statistics of this <see cref="Operation"/>.
+	/// </summary>
+	public unsafe T Statistics => default(T).Sum(array.Pointer, statisticsCount);
+
+	public override void Prepare(int population)
+	{
+		base.Prepare(population);
+
+		if (array == null || array.Length < population)
+		{
+			array?.Dispose();
+			array = new AlignedArray<T>(population);
+		}
+		else array.Clear();
+
+		statisticsCount = population;
+	}
+
+	protected sealed override void Execute(ulong procedure, IScheduler scheduler)
+	{
+		ref T statistics = ref array[(int)procedure];
+		Execute(procedure, scheduler, ref statistics);
+	}
+
+	/// <inheritdoc cref="Execute(ulong, Echo.Common.Compute.IScheduler)"/>
+	/// <param name="statistics">The <see cref="IStatistics{T}"/> to be used with this execution.</param>
+	// ReSharper disable InvalidXmlDocComment
+	protected abstract void Execute(ulong procedure, IScheduler scheduler, ref T statistics);
+	// ReSharper restore InvalidXmlDocComment
+
+	protected override void Dispose(bool disposing)
+	{
+		base.Dispose(disposing);
+		if (disposing) array?.Dispose();
+	}
 }
---
+++ Echo/src/Core/Evaluation/Operations/EvaluationStatistics.cs
@@ -0,0 +1,9 @@
+﻿using Echo.Common.Compute;
+
+namespace Echo.Core.Evaluation.Operations;
+
+/// <summary>
+/// <see cref="IStatistics{T}"/> type used by the evaluation systems.
+/// </summary>
+[GeneratedStatistics]
+public partial struct EvaluationStatistics : IStatistics<EvaluationStatistics> { }
