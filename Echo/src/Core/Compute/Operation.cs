430bf923e334280c0645960ba96f9a6e63c87a9c
Gary Huang <henhaodegaigai@gmail.com>
2022-05-28 15:41:56 -0400

    Changed Operation to be more deterministic

--- Echo/src/Core/Compute/Operation.cs
+++ Echo/src/Core/Compute/Operation.cs
@@ -3,11 +3,12 @@
 namespace Echo.Core.Compute;
 
 /// <summary>
-/// A task with individual steps to be executed concurrently.
+/// A task with individual steps to be executed both sequentially and concurrently.
 /// </summary>
 public abstract class Operation
 {
-	ulong currentProcedure;
+	ulong nextProcedure;
+	ulong procedureCount;
 
 	/// <summary>
 	/// Prepares this <see cref="Operation"/> for execution.
@@ -15,29 +16,36 @@ public abstract class Operation
 	/// <param name="population">The maximum number of concurrent <see cref="IScheduler"/> executing. Note that
 	/// all <see cref="IScheduler.Id"/> will be between zero (inclusive) and this value (exclusive).</param>
 	/// <remarks>This method is invoked once before all execution begins</remarks>
-	public virtual void Prepare(int population)
+	public void Prepare(int population)
 	{
-		Interlocked.Exchange(ref currentProcedure, 0);
+		Interlocked.Exchange(ref nextProcedure, unchecked((ulong)0 - 1));
+		procedureCount = WarmUp(population);
 	}
 
 	/// <summary>
 	/// Joins the execution of this <see cref="Operation"/> once.
 	/// </summary>
-	/// <param name="scheduler">The <see cref="IScheduler"/> used.</param>
+	/// <param name="scheduler">The <see cref="IScheduler"/> to use.</param>
 	/// <returns>Whether this execution performed any work.</returns>
 	public bool Execute(IScheduler scheduler)
 	{
-		ulong procedure = Interlocked.Increment(ref currentProcedure);
-		return Execute(procedure - 1, scheduler);
+		ulong procedure = Interlocked.Increment(ref nextProcedure);
+		if (procedure >= procedureCount) return false;
+
+		Execute(procedure, scheduler);
+		return true;
 	}
+
+	/// <inheritdoc cref="Prepare"/>
+	/// <returns>The number of steps the entire <see cref="Operation"/> has.</returns>
+	protected abstract ulong WarmUp(int population);
 
 	/// <summary>
 	/// Executes one step of this <see cref="Operation"/>.
 	/// </summary>
-	/// <param name="procedure">The number/index of the step to execute.</param>
-	/// <param name="scheduler">The <see cref="IScheduler"/> used.</param>
-	/// <returns>Whether this step performed any work.</returns>
-	/// <remarks>Once this method returns false, most steps after this one will not be executed.
-	/// However, it is not guaranteed that ALL later steps will not be executed.</remarks>
-	protected abstract bool Execute(ulong procedure, IScheduler scheduler);
+	/// <param name="procedure">The number/index of the step to execute; this value is between 0 (inclusive)
+	/// and the returned number from <see cref="WarmUp"/> (exclusive). This value will gradually increase as
+	/// this <see cref="Operation"/> gets completed.</param>
+	/// <param name="scheduler">The to <see cref="IScheduler"/> use.</param>
+	protected abstract void Execute(ulong procedure, IScheduler scheduler);
 }
