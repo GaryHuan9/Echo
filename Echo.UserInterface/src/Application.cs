c6498fd81a7eb315258effaa2f80b8d2a440eacc
Gary Huang <henhaodegaigai@gmail.com>
2022-05-30 19:15:46 -0400

    Dear ImGui working with SDL2!

---
+++ Echo.UserInterface/src/Application.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ImGuiNET;
+
+namespace Echo.UserInterface;
+
+public sealed class Application : IDisposable
+{
+	public void Initialize() { }
+
+	public void Update()
+	{
+		// if (ImGui.Begin("Test Window")) { }
+		ImGuiIOPtr io = ImGui.GetIO();
+
+		ImGui.ShowDemoWindow();
+	}
+
+	public void Dispose() { }
+}
