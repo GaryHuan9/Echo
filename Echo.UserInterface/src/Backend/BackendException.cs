3226b9bbfe71a8a95df7fa598d96ac104ba3ee5c
Gary Huang <henhaodegaigai@gmail.com>
2022-05-30 23:34:16 -0400

    Refactoring Dear ImGui backend

---
+++ Echo.UserInterface/src/Backend/BackendException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Echo.UserInterface.Backend;
+
+public sealed class BackendException : Exception
+{
+	public BackendException() : base("An error occured from the SDL2 backend.") { }
+	public BackendException(int code) : this((long)code) { }
+	public BackendException(uint code) : this((long)code) { }
+	BackendException(long code) : base($"An error occured from the SDL2 backend (Error code: {code}).") { }
+}
+
+public static class BackendExceptionExtensions
+{
+	public static void ThrowOnError(this int code)
+	{
+		if (code == 0) return;
+		throw new BackendException(code);
+	}
+
+	public static void ThrowOnError(this uint code)
+	{
+		if (code == 0) return;
+		throw new BackendException(code);
+	}
+}
---
+++ Echo.UserInterface/src/Backend/IApplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Echo.UserInterface.Backend;
+
+public interface IApplication : IDisposable
+{
+	TimeSpan UpdateDelay { get; }
+
+	string Label { get; }
+
+	void Initialize();
+
+	void Update();
+}
