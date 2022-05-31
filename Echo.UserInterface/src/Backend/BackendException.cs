585f9811aed0aeafcb5e541a55c8b73f3fa2fb1a
Gary Huang <henhaodegaigai@gmail.com>
2022-05-31 19:41:04 -0400

    Removed errors

--- Echo.UserInterface/src/Backend/BackendException.cs
+++ Echo.UserInterface/src/Backend/BackendException.cs
@@ -5,22 +5,14 @@ namespace Echo.UserInterface.Backend;
 public sealed class BackendException : Exception
 {
 	public BackendException() : base("An error occured from the SDL2 backend.") { }
-	public BackendException(int code) : this((long)code) { }
-	public BackendException(uint code) : this((long)code) { }
-	BackendException(long code) : base($"An error occured from the SDL2 backend (Error code: {code}).") { }
+	public BackendException(int code) : base($"An error occured from the SDL2 backend (Error code: {code}).") { }
 }
 
 public static class BackendExceptionExtensions
 {
 	public static void ThrowOnError(this int code)
 	{
-		if (code == 0) return;
-		throw new BackendException(code);
-	}
-
-	public static void ThrowOnError(this uint code)
-	{
-		if (code == 0) return;
+		if (code >= 0) return;
 		throw new BackendException(code);
 	}
 }
