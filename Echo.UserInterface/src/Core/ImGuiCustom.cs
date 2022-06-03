529dd0ca478b29a4fc6c9e8e4bbe0f2f08fa8a08
Gary Huang <henhaodegaigai@gmail.com>
2022-06-02 22:34:08 -0400

    Added great information display

---
+++ Echo.UserInterface/src/Core/ImGuiCustom.cs
@@ -0,0 +1,23 @@
+﻿using ImGuiNET;
+
+namespace Echo.UserInterface.Core;
+
+public static class ImGuiCustom
+{
+	public static void TableItem(string value, bool wrap = false)
+	{
+		ImGui.TableNextColumn();
+		if (wrap) ImGui.TextWrapped(value);
+		else ImGui.TextUnformatted(value);
+	}
+
+	public static bool BeginProperties(string name) => ImGui.BeginTable(name, 2, ImGuiTableFlags.BordersOuter);
+
+	public static void Property(string label, string value)
+	{
+		TableItem(label);
+		TableItem(value);
+	}
+
+	public static void EndProperties() => ImGui.EndTable();
+}
