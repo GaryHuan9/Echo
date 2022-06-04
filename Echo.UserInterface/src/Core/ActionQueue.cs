e724c1f722d1e6ac70f6fc381164a0954383e62b
Gary Huang <henhaodegaigai@gmail.com>
2022-06-03 23:17:26 -0400

    Added ActionQueue for user interface

---
+++ Echo.UserInterface/src/Core/ActionQueue.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using CodeHelpers.Collections;
+using CodeHelpers.Diagnostics;
+using Echo.Common;
+
+namespace Echo.UserInterface.Core;
+
+public static class ActionQueue
+{
+	static readonly BlockingCollection<Labeled<Action>> queue = new();
+	static ConcurrentList<Labeled<Record>> history = new();
+
+	static readonly HashSet<string> labels = new();
+	static readonly Locker locker = new();
+	static bool hasThread;
+
+	public static IReadOnlyList<Labeled<Record>> History => history;
+
+	public static void Enqueue(Action action, string label)
+	{
+		using var _ = locker.Fetch();
+
+		if (!labels.Add(label))
+		{
+			AddRecord(label, EventType.EnqueuedDuplicate);
+			return;
+		}
+
+		AddRecord(label, EventType.EnqueueSucceed);
+		queue.Add(new Labeled<Action>(label, action));
+
+		if (hasThread) return;
+
+		var thread = new Thread(ProcessQueue) { IsBackground = true };
+		hasThread = true;
+		thread.Start();
+	}
+
+	public static void ClearHistory() => history = new ConcurrentList<Labeled<Record>>();
+
+	static void ProcessQueue()
+	{
+		foreach ((string label, Action action) in queue.GetConsumingEnumerable())
+		{
+			AddRecord(label, EventType.DequeueStarted);
+
+			action();
+
+			AddRecord(label, EventType.DequeueCompleted);
+
+			bool removed = labels.Remove(label);
+			Assert.IsTrue(removed);
+		}
+	}
+
+	static void AddRecord(string label, EventType type) => history.ImmediateAdd(new Labeled<Record>(label, new Record(DateTime.Now, type)));
+
+	public enum EventType
+	{
+		EnqueueSucceed,
+		EnqueuedDuplicate,
+		DequeueStarted,
+		DequeueCompleted
+	}
+
+	public readonly record struct Labeled<T>(string Label, in T Item);
+	public readonly record struct Record(DateTime Time, EventType Type);
+}
---
+++ Echo.UserInterface/src/Core/ActionsUI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ImGuiNET;
+
+namespace Echo.UserInterface.Core;
+
+public class ActionsUI : AreaUI
+{
+	public ActionsUI() : base("Actions") { }
+
+	protected override void Draw()
+	{
+		if (ImGui.Button("Clear All")) ActionQueue.ClearHistory();
+
+		ImGui.Separator();
+
+		foreach ((string label, (DateTime time, ActionQueue.EventType type)) in ActionQueue.History)
+		{
+			string text = type switch
+			{
+				ActionQueue.EventType.EnqueueSucceed => $"Successfully enqueued `{label}`",
+				ActionQueue.EventType.EnqueuedDuplicate => $"Attempting to enqueue duplicate `{label}`",
+				ActionQueue.EventType.DequeueStarted => $"Started executing `{label}`",
+				ActionQueue.EventType.DequeueCompleted => $"Completed executing `{label}`",
+				_ => throw new ArgumentOutOfRangeException()
+			};
+
+			ImGui.TextUnformatted($"[{time.ToStringDefault()}]: {text}");
+		}
+	}
+}
