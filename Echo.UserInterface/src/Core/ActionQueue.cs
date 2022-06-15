0835b52d7e2a2ad13cd23be1145d67acc9b91f61
Gary Huang <henhaodegaigai@gmail.com>
2022-06-14 23:37:39 -0400

    Adding LogsUI

--- Echo.UserInterface/src/Core/ActionQueue.cs
+++ Echo.UserInterface/src/Core/ActionQueue.cs
@@ -11,13 +11,10 @@ namespace Echo.UserInterface.Core;
 public static class ActionQueue
 {
 	static readonly BlockingCollection<Labeled<Action>> queue = new();
-	static ConcurrentList<Labeled<Record>> history = new();
-
 	static readonly HashSet<string> labels = new();
 	static readonly Locker locker = new();
-	static bool hasThread;
 
-	public static IReadOnlyList<Labeled<Record>> History => history;
+	static bool hasThread;
 
 	public static void Enqueue(Action action, string label)
 	{
@@ -25,11 +22,11 @@ public static class ActionQueue
 
 		if (!labels.Add(label))
 		{
-			AddRecord(label, EventType.EnqueuedDuplicate);
+			LogList.Add($"Attempting to enqueue duplicate '{label}' into {nameof(ActionQueue)}.");
 			return;
 		}
 
-		AddRecord(label, EventType.EnqueueSucceed);
+		LogList.Add($"Successfully enqueued '{label}' into {nameof(ActionQueue)}.");
 		queue.Add(new Labeled<Action>(label, action));
 
 		if (hasThread) return;
@@ -39,33 +36,21 @@ public static class ActionQueue
 		thread.Start();
 	}
 
-	public static void ClearHistory() => history = new ConcurrentList<Labeled<Record>>();
-
 	static void ProcessQueue()
 	{
 		foreach ((string label, Action action) in queue.GetConsumingEnumerable())
 		{
-			AddRecord(label, EventType.DequeueStarted);
+			LogList.Add($"Dequeued and began executing '{label}' from {nameof(ActionQueue)}.");
 
 			action();
 
-			AddRecord(label, EventType.DequeueCompleted);
+			LogList.Add($"Completed executing '{label}' from {nameof(ActionQueue)}.");
 
+			// ReSharper disable once RedundantAssignment
 			bool removed = labels.Remove(label);
 			Assert.IsTrue(removed);
 		}
 	}
 
-	static void AddRecord(string label, EventType type) => history.ImmediateAdd(new Labeled<Record>(label, new Record(DateTime.Now, type)));
-
-	public enum EventType
-	{
-		EnqueueSucceed,
-		EnqueuedDuplicate,
-		DequeueStarted,
-		DequeueCompleted
-	}
-
-	public readonly record struct Labeled<T>(string Label, in T Item);
-	public readonly record struct Record(DateTime Time, EventType Type);
+	readonly record struct Labeled<T>(string Label, in T Item);
 }
---
+++ Echo.UserInterface/src/Core/Areas/LogsUI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CodeHelpers.Diagnostics;
+using Echo.UserInterface.Backend;
+using ImGuiNET;
+
+namespace Echo.UserInterface.Core.Areas;
+
+public class LogsUI : AreaUI
+{
+	public LogsUI() : base("Logs") { }
+
+	public override void Initialize()
+	{
+		base.Initialize();
+		DebugHelper.Logger = LogList.Logger;
+	}
+
+	protected override void Update(in Moment moment)
+	{
+		if (ImGui.Button("Clear All")) LogList.Clear();
+
+		ImGui.SameLine();
+		if (ImGui.Button("Save to File")) throw new NotImplementedException();
+
+		ImGui.Separator();
+		ImGui.BeginChild("History");
+
+		foreach (string log in LogList.Logs) ImGui.TextUnformatted(log);
+
+		ImGui.EndChild();
+	}
+}
