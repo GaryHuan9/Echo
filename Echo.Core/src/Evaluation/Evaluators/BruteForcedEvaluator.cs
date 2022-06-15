143c31d754fbf3a6551a8032a6231db66d1ec3e8
ClemensU42 <clemens_u@web.de>
2022-06-15 19:05:48 +0200

    started work on BruteForcedEvaluator

--- Echo.Core/src/Evaluation/Evaluators/BruteForcedEvaluator.cs
+++ Echo.Core/src/Evaluation/Evaluators/BruteForcedEvaluator.cs
@@ -1,51 +1,58 @@
-﻿// using CodeHelpers.Packed;
-// using Echo.Common.Mathematics;
-// using Echo.Common.Mathematics.Primitives;
-// using Echo.Common.Memory;
-// using Echo.Core.Aggregation.Primitives;
-// using Echo.Core.Evaluation.Materials;
-// using Echo.Core.Evaluation.Scattering;
-// using Echo.Core.Textures.Colors;
+﻿using System;
+using CodeHelpers.Packed;
+using Echo.Core.Common.Mathematics;
+using Echo.Core.Common.Mathematics.Primitives;
+using Echo.Core.Common.Memory;
+using Echo.Core.Aggregation.Primitives;
+using Echo.Core.Evaluation.Materials;
+using Echo.Core.Evaluation.Scattering;
+using Echo.Core.Textures.Colors;
+using Echo.Core.Scenic.Preparation;
+using Echo.Core.Evaluation.Distributions.Continuous;
 
-// namespace Echo.Core.Evaluation.Evaluators;
+namespace Echo.Core.Evaluation.Evaluators;
 
-// public class BruteForcedEvaluator : PathTracedEvaluator //Interesting inheritance, we will probably remove this later
-// {
-// 	public override RGB128 Evaluate(in Ray ray, RenderProfile profile, Arena arena)
-// 	{
-// 		var scene = profile.Scene;
+public record BruteForcedEvaluator : PathTracedEvaluator //Interesting inheritance, we will probably remove this later
+{
 
-// 		var energy = RGB128.White;
-// 		var radiant = RGB128.Black;
+    public int bounceLimit = 4;
 
-// 		var query = new TraceQuery(ray);
+    public override Float4 Evaluate(PreparedScene scene, in Ray ray, ContinuousDistribution distribution, Allocator allocator)
+    {
+        var query = new TraceQuery(ray);
+        RGB128 resultColor = RGB128.Black;
+        int collisions = 0;
 
-// 		for (int bounce = 0; bounce < BounceLimit; bounce++)
-// 		{
-// 			if (!profile.Scene.Trace(ref query)) break;
-// 			using var _ = arena.allocator.Begin();
+        for (int i = 0; i < bounceLimit; i++)
+        {
 
-// 			Touch touch = profile.Scene.Interact(query);
-// 			touch.shade.material.Scatter(ref touch, arena.allocator);
+            while (scene.Trace(ref query))
+            {
+                collisions++;
+                Touch touch = scene.Interact(query);
 
-// 			if (touch.bsdf == null)
-// 			{
-// 				query = query.SpawnTrace();
-// 				continue;
-// 			}
+                allocator.Restart();
 
-// 			(RGB128 scatter, float pdf) = touch.bsdf.Sample(touch.outgoing, arena.Distribution.Next2D(), out Float3 incident, out BxDF function);
-// 			if (touch.shade.material is IEmissive emissive && FastMath.Positive(emissive.Power)) radiant += energy * emissive.Emit(touch.point, touch.outgoing);
+                var material = touch.shade.material;
 
-// 			if (!FastMath.Positive(pdf) | scatter.IsZero) energy = RGB128.Black;
-// 			else energy *= touch.NormalDot(incident) / pdf * scatter;
+                material.Scatter(ref touch, allocator);
 
-// 			if (energy.IsZero) break;
-// 			query = query.SpawnTrace(incident);
-// 		}
+                if (touch.bsdf != null)
+                {
+                    float exp = MathF.Pow(0.5f, i);
+                    resultColor += (RGB128)material.SampleAlbedo(touch) * new RGB128(exp, exp, exp);
 
-// 		if (!energy.IsZero) radiant += energy * scene.lights.EvaluateAmbient(query.ray.direction);
+                    query = touch.SpawnTrace();
+                    break;
+                }
 
-// 		return radiant;
-// 	}
-// }
+                query = query.SpawnTrace();
+
+            }
+
+        }
+        //if (collisions == 0) return scene.lights.EvaluateAmbient(query.ray.direction);
+
+        return resultColor;
+    }
+}
