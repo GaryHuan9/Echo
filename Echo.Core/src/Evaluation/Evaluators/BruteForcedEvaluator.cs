ff20356f585d7a5c818fa0b2445d2033faefe8f8
ClemensU42 <clemens_u@web.de>
2022-06-16 22:17:50 +0200

    added mostly correct rendering equation to BruteForcedEval

--- Echo.Core/src/Evaluation/Evaluators/BruteForcedEvaluator.cs
+++ Echo.Core/src/Evaluation/Evaluators/BruteForcedEvaluator.cs
@@ -14,41 +14,41 @@ namespace Echo.Core.Evaluation.Evaluators;
 public record BruteForcedEvaluator : Evaluator
 {
 
-    public int bounceLimit = 16;
+	public int bounceLimit = 32;
 
-    public override Float4 Evaluate(PreparedScene scene, in Ray ray, ContinuousDistribution distribution, Allocator allocator)
-    {
-        return Evaluate(scene, ray, distribution, allocator, bounceLimit);
-    }
+	public override Float4 Evaluate(PreparedScene scene, in Ray ray, ContinuousDistribution distribution, Allocator allocator)
+	{
+		return Evaluate(scene, ray, distribution, allocator, bounceLimit);
+	}
 
-    public override IEvaluationLayer CreateOrClearLayer(RenderBuffer buffer) => CreateOrClearLayer<RGB128>(buffer, "path");
+	public override IEvaluationLayer CreateOrClearLayer(RenderBuffer buffer) => CreateOrClearLayer<RGB128>(buffer, "path");
 
-    private Float4 Evaluate(PreparedScene scene, in Ray ray, ContinuousDistribution distribution, Allocator allocator, int depth)
-    {
-        if (depth <= 0) return RGB128.Black;
+	private Float4 Evaluate(PreparedScene scene, in Ray ray, ContinuousDistribution distribution, Allocator allocator, int depth)
+	{
+		if (depth <= 0) return RGB128.Black;
 
-        var query = new TraceQuery(ray);
+		var query = new TraceQuery(ray);
 
-        allocator.Restart();
+		allocator.Restart();
 
-        while (scene.Trace(ref query))
-        {
-            Touch touch = scene.Interact(query);
+		while (scene.Trace(ref query))
+		{
+			Touch touch = scene.Interact(query);
 
-            var material = touch.shade.material;
-            material.Scatter(ref touch, allocator);
-            if (touch.bsdf != null)
-            {
-                Float3 incidentWorld = Float3.Zero;
-                BxDF bxdf = null;
-                touch.bsdf.Sample(touch.outgoing, distribution.Next2D(), out incidentWorld, out bxdf);
-                return (material.SampleAlbedo(touch) + Evaluate(scene, query.SpawnTrace(incidentWorld).ray, distribution, allocator, depth - 1)) * 0.5f;
-            }
+			var material = touch.shade.material;
+			material.Scatter(ref touch, allocator);
+			if (touch.bsdf != null)
+			{
+				Float3 incidentWorld = Float3.Zero;
+				Probable<RGB128> sample = touch.bsdf.Sample(touch.outgoing, distribution.Next2D(), out incidentWorld, out _);
+				RGB128 color = sample.content / sample.pdf;
+				return (color * Evaluate(scene, query.SpawnTrace(incidentWorld).ray, distribution, allocator, depth - 1)) * touch.NormalDot(incidentWorld);
+			}
 
-            query = query.SpawnTrace();
-        }
+			query = query.SpawnTrace();
+		}
 
-        return scene.lights.EvaluateAmbient(query.ray.direction);
+		return scene.lights.EvaluateAmbient(query.ray.direction);
 
-    }
+	}
 }
