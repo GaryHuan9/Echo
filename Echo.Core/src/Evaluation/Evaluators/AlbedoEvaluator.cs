a4b95aa7ae937c5a65d322a52f84f9a5c29fab85
ClemensU42 <clemens_u@web.de>
2022-06-18 21:14:49 +0200

    Fixed merge issues
    Former-commit-id: d22948b8bd7b626c0b1b2c94e73877847bdc8e69

--- Echo.Core/src/Evaluation/Evaluators/AlbedoEvaluator.cs
+++ Echo.Core/src/Evaluation/Evaluators/AlbedoEvaluator.cs
@@ -5,71 +5,37 @@ using Echo.Core.Textures.Colors;
 using Echo.Core.Scenic.Preparation;
 using Echo.Core.Evaluation.Distributions.Continuous;
 using CodeHelpers.Packed;
-<<<<<<< HEAD
-using Echo.Core.Textures.Evaluation;
-using System.Collections.Generic;
-=======
 using Echo.Core.Evaluation.Materials;
 using Echo.Core.Textures.Evaluation;
->>>>>>> origin/main
+using System.Collections.Generic;
 
 namespace Echo.Core.Evaluation.Evaluators;
 
 public record AlbedoEvaluator : Evaluator
 {
-    public override IEvaluationLayer CreateOrClearLayer(RenderBuffer buffer) => CreateOrClearLayer<RGB128>(buffer, "albedo");
+	public override IEvaluationLayer CreateOrClearLayer(RenderBuffer buffer) => CreateOrClearLayer<RGB128>(buffer, "albedo");
 
-    public override Float4 Evaluate(PreparedScene scene, in Ray ray, ContinuousDistribution distribution, Allocator allocator)
-    {
-        var query = new TraceQuery(ray);
+	public override Float4 Evaluate(PreparedScene scene, in Ray ray, ContinuousDistribution distribution, Allocator allocator)
+	{
+		var query = new TraceQuery(ray);
 
-        List<RGBA128> colors = new List<RGBA128>();
+		List<RGBA128> colors = new List<RGBA128>();
 
-        //Trace for intersection
-        while (scene.Trace(ref query))
-        {
-            Touch touch = scene.Interact(query);
+		//Trace for intersection
+		while (scene.Trace(ref query))
+		{
+			Touch touch = scene.Interact(query);
 
-            allocator.Restart();
+			allocator.Restart();
 
-            Material material = touch.shade.material;
-            material.Scatter(ref touch, allocator);
+			Material material = touch.shade.material;
+			material.Scatter(ref touch, allocator);
 
-            if (touch.bsdf == null) query = query.SpawnTrace();
-<<<<<<< HEAD
-            else
-            {
-                RGBA128 color = touch.shade.material.SampleAlbedo(touch);
-                colors.Add(color);
-                if (color.Alpha == 1f) return MixColors(colors);
-                else query = query.SpawnTrace();
-            }
+			if (touch.bsdf == null) query = query.SpawnTrace();
+			else return (RGB128)material.SampleAlbedo(touch);
+		}
 
-        }
-
-        //Sample ambient
-        RGB128 ambient = scene.lights.EvaluateAmbient(query.ray.direction);
-        colors.Add((RGBA128)ambient);
-        return MixColors(colors);
-    }
-
-    private RGB128 MixColors(List<RGBA128> colors)
-    {
-        // Due to the last color not being transparent we use it as a base
-        RGB128 result = (RGB128)colors[colors.Count - 1];
-
-        for (int i = colors.Count - 2; i >= 0; i--)
-        {
-            result = (result * (1f - colors[i].Alpha)) + ((RGB128)colors[i] * colors[i].Alpha);
-        }
-
-        return result;
-=======
-            else return (RGB128)material.SampleAlbedo(touch);
-        }
-
-        //Sample ambient
-        return scene.lights.EvaluateAmbient(query.ray.direction);
->>>>>>> origin/main
-    }
+		//Sample ambient
+		return scene.lights.EvaluateAmbient(query.ray.direction);
+	}
 }
--- Echo.UserInterface/src/Core/Areas/SystemUI.cs
+++ Echo.UserInterface/src/Core/Areas/SystemUI.cs
@@ -250,54 +250,30 @@ public class SystemUI : AreaUI
 	static string GetCompilerMode()
 	{
 #if DEBUG
-        return "DEBUG";
+		return "DEBUG";
 #elif RELEASE
 		return "RELEASE";
 #else
-        return "Unknown";
+		return "Unknown";
 #endif
 	}
 
 	static void DispatchDevice(Device device) => ActionQueue.Enqueue("Evaluation Operation Dispatch", () =>
 	{
 		var scene = new SingleBunny();
+		//var scene = new CornellBox();
 
-<<<<<<< HEAD
-		void Dispatch()
-		{
-			var scene = new SingleBunny();
-			//var scene = new CornellBox();
-
-			var prepareProfile = new ScenePrepareProfile();
-
-			var evaluationProfile = new EvaluationProfile
-			{
-				Scene = new PreparedScene(scene, prepareProfile),
-				Evaluator = new BruteForcedEvaluator(),
-				Distribution = new StratifiedDistribution { Extend = 16 },
-				Buffer = new RenderBuffer(new Int2(640, 480)),
-				Pattern = new SpiralPattern(),
-				MinEpoch = 1,
-				MaxEpoch = 20
-			};
-
-			var operation = new EvaluationOperation.Factory
-			{
-				NextProfile = evaluationProfile
-			};
-=======
 		var prepareProfile = new ScenePrepareProfile();
->>>>>>> origin/main
 
 		var evaluationProfile = new EvaluationProfile
 		{
 			Scene = new PreparedScene(scene, prepareProfile),
-			Evaluator = new PathTracedEvaluator(),
+			Evaluator = new BruteForcedEvaluator(),
 			Distribution = new StratifiedDistribution { Extend = 16 },
-			Buffer = new RenderBuffer(new Int2(960, 540)),
+			Buffer = new RenderBuffer(new Int2(640, 480)),
 			Pattern = new SpiralPattern(),
 			MinEpoch = 1,
-			MaxEpoch = 4
+			MaxEpoch = 20
 		};
 
 		var operation = new EvaluationOperation.Factory
@@ -306,5 +282,6 @@ public class SystemUI : AreaUI
 		};
 
 		device.Dispatch(operation);
+
 	});
 }
