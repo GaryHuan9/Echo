e6522d316962641435d7aa1ad3c9686893b1fb60
ClemensU42 <clemens_u@web.de>
2022-06-18 22:57:43 +0200

    reverted changes in AggregatorQualityEvaluator
    Former-commit-id: b9b080cf2ff922e651c863cd20321fe17a6a0d9e

--- Echo.Core/src/Evaluation/Evaluators/AggregatorQualityEvaluator.cs
+++ Echo.Core/src/Evaluation/Evaluators/AggregatorQualityEvaluator.cs
@@ -1,37 +1,37 @@
-﻿using System.Threading;
-using Echo.Core.Common.Mathematics.Primitives;
-using Echo.Core.Common.Memory;
-using Echo.Core.Evaluation.Distributions.Continuous;
-using Echo.Core.Textures.Colors;
-using Echo.Core.Textures.Evaluation;
-using Echo.Core.Scenic.Preparation;
-using Echo.Core.Evaluation.Operations;
-using CodeHelpers.Packed;
+﻿// using System.Threading;
+// using Echo.Core.Common.Mathematics.Primitives;
+// using Echo.Core.Common.Memory;
+// using Echo.Core.Evaluation.Distributions.Continuous;
+// using Echo.Core.Textures.Colors;
+// using Echo.Core.Textures.Evaluation;
+// using Echo.Core.Scenic.Preparation;
+// using Echo.Core.Evaluation.Operations;
+// using CodeHelpers.Packed;
 
-namespace Echo.Core.Evaluation.Evaluators;
+// namespace Echo.Core.Evaluation.Evaluators;
 
-public record AggregatorQualityEvaluator : Evaluator
-{
-	long totalCost;
-	long totalSample;
+// public record AggregatorQualityEvaluator : Evaluator
+// {
+// 	long totalCost;
+// 	long totalSample;
 
-	public override Float4 Evaluate(PreparedScene scene, in Ray ray, ContinuousDistribution distribution, Allocator allocator)
-	{
-		uint cost = scene.TraceCost(ray);
+// 	public override Float4 Evaluate(PreparedScene scene, in Ray ray, ContinuousDistribution distribution, Allocator allocator)
+// 	{
+// 		uint cost = scene.TraceCost(ray);
 
-		long currentCost = Interlocked.Add(ref totalCost, cost);
-		long currentSample = Interlocked.Increment(ref totalSample);
+// 		long currentCost = Interlocked.Add(ref totalCost, cost);
+// 		long currentSample = Interlocked.Increment(ref totalSample);
 
-		return new RGB128(cost, currentCost, currentSample);
-	}
+// 		return new RGB128(cost, currentCost, currentSample);
+// 	}
 
-	// protected override ContinuousDistribution CreateDistribution(Echo.Core.Evaluation.Engines.RenderProfile profile)
-	// {
-	//     Interlocked.Exchange(ref totalCost, 0);
-	//     Interlocked.Exchange(ref totalSample, 0);
+// 	protected override ContinuousDistribution CreateDistribution(Echo.Core.Evaluation.Engines.RenderProfile profile)
+// 	{
+// 	    Interlocked.Exchange(ref totalCost, 0);
+// 	    Interlocked.Exchange(ref totalSample, 0);
 
-	//     return base.CreateDistribution(profile);
-	// }
+// 	    return base.CreateDistribution(profile);
+// 	}
 
-	public override IEvaluationLayer CreateOrClearLayer(RenderBuffer buffer) => CreateOrClearLayer<RGB128>(buffer, "aggregator_quality");
-}
+// 	public override IEvaluationLayer CreateOrClearLayer(RenderBuffer buffer) => CreateOrClearLayer<RGB128>(buffer, "aggregator_quality");
+// }
