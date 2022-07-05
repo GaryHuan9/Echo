ff41af465ee04992ac0ab95452f640b46ad47cde
Clemens Ulrich <clemens_u@web.de>
2022-07-05 20:12:55 +0200

    Moved header reading into header struct and improved ReadLine function
    Former-commit-id: df88f6d381e9e5b63c5da3ebfe66d41e9d785aa7

--- Echo.Core/src/InOut/PLYModelLoader.cs
+++ Echo.Core/src/InOut/PLYModelLoader.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using CodeHelpers.Packed;
+using Echo.Core.Common;
 
 namespace Echo.Core.InOut;
 
@@ -10,63 +12,17 @@ public class PLYModelLoader
 {
 	public PLYModelLoader(string path)
 	{
-		_file = new FileStream(path, FileMode.Open);
+		file = new FileStream(path, FileMode.Open);
 
 		// check for magic number
 
-		string line = ReadLine();
+		string line = ReadLine(file, ref buffer);
 		if (line != "ply") throw new Exception($"Error loading {path}");
 
 		// read the header
-		int propertyIndex = 0;
-		header.propertiesPositions = new Dictionary<Properties, int>();
-		while (true)
-		{
-			string[] tokens = ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
-			switch (tokens[0])
-			{
-				case "comment":
-					continue;
-				case "format":
-					header.format = tokens[1] switch
-					{
-						"ascii"                => Format.ASCII,
-						"binary_little_endian" => Format.BINARY_LITTLE_ENDIAN,
-						"binary_big_endian"    => Format.BINARY_BIG_ENDIAN,
-						_                      => throw new Exception($"PLY specified format {tokens[1]} is invalid!")
-					};
-					break;
-				case "element":
-					if (tokens[1] == "vertex") header.vertexAmount = int.Parse(tokens[2]);
-					else if (tokens[1] == "face") header.faceAmount = int.Parse(tokens[2]);
-					break;
-				case "property":
-					if (tokens[1] == "list")
-					{
-						break;
-					}
-
-					header.propertiesPositions.Add(
-						tokens[2] switch
-						{
-							"x"  => Properties.X,
-							"y"  => Properties.Y,
-							"z"  => Properties.Z,
-							"nx" => Properties.NX,
-							"ny" => Properties.NY,
-							"nz" => Properties.NZ,
-							"s"  => Properties.S,
-							"t"  => Properties.T
-						},
-						propertyIndex++
-					);
-					break;
-				case "end_header":
-					goto header_finished;
-			}
-		}
-
-	header_finished: ;
+		header = new Header(ref file, ref buffer);
+		file.Position = header.faceListStart;
+		Console.WriteLine(ReadLine(file, ref buffer));
 	}
 
 	public Triangle ReadTriangle()
@@ -74,29 +30,31 @@ public class PLYModelLoader
 		throw new NotImplementedException();
 	}
 
-	string ReadLine()
+	static string ReadLine(FileStream file, ref byte[] buffer)
 	{
-		buffer.Clear();
-		for (byte currentChar = 0x00; Convert.ToChar(currentChar) != '\n';)
+		int stringSize = 0;
+		int i = 0;
+		while (true)
 		{
-			currentChar = (byte)_file.ReadByte();
-			buffer.Add(Convert.ToChar(currentChar));
+			Utility.EnsureCapacity(ref buffer, i + 1, true, buffer.Length * 2);
+			buffer[i] = (byte)file.ReadByte();
+			stringSize++;
+			if (Convert.ToChar(buffer[i++]) == '\n') break;
 		}
 
-		// remove the \n char at the end as it's not needed
-		buffer.RemoveAt(buffer.Count - 1);
-		return new(buffer.ToArray());
+		string result = Encoding.ASCII.GetString(buffer, 0, stringSize - 1); // stringSize - 1 to remove the \n
+		return result;
 	}
 
-	readonly List<char> buffer = new();
-	readonly FileStream _file;
+	readonly byte[] buffer = new byte[1];
+	readonly FileStream file;
 	Header header;
 
 	public struct Triangle
 	{
-		public Float3[] vertices;
-		public Float3[] normals;
-		public Float2[] texcoords;
+		public Float3 vertex0, vertex1, vertex2;
+		public Float3 normal0, normal1, normal2;
+		public Float2 texcoord0, texcoord1;
 	}
 
 	struct Header
@@ -105,6 +63,130 @@ public class PLYModelLoader
 		public int vertexAmount;
 		public int faceAmount;
 		public Dictionary<Properties, int> propertiesPositions;
+
+		public long vertexListStart;
+		public long faceListStart;
+
+		int vertexSize = 0;
+
+		public Header(ref FileStream file, ref byte[] buffer)
+		{
+			int propertyIndex = 0;
+			propertiesPositions = new Dictionary<Properties, int>();
+			format = Format.ASCII;
+			vertexAmount = 0;
+			faceAmount = 0;
+			while (true)
+			{
+				string[] tokens = ReadLine(file, ref buffer).Split(' ', StringSplitOptions.RemoveEmptyEntries);
+				switch (tokens[0])
+				{
+					case "comment":
+						continue;
+					case "format":
+						format = tokens[1] switch
+						{
+							"ascii"                => Format.ASCII,
+							"binary_little_endian" => Format.BINARY_LITTLE_ENDIAN,
+							"binary_big_endian"    => Format.BINARY_BIG_ENDIAN,
+							_                      => throw new Exception($"PLY specified format {tokens[1]} is invalid!")
+						};
+						break;
+					case "element":
+						switch (tokens[1])
+						{
+							case "vertex":
+								vertexAmount = int.Parse(tokens[2]);
+								break;
+							case "face":
+								faceAmount = int.Parse(tokens[2]);
+								break;
+						}
+
+						break;
+					case "property":
+						if (tokens[1] == "list")
+						{
+							break;
+						}
+
+						switch (tokens[2])
+						{
+							case "x":
+								propertiesPositions.Add(Properties.X, propertyIndex++);
+								vertexSize += sizeof(float);
+								break;
+							case "y":
+								propertiesPositions.Add(Properties.Y, propertyIndex++);
+								vertexSize += sizeof(float);
+								break;
+							case "z":
+								propertiesPositions.Add(Properties.Z, propertyIndex++);
+								vertexSize += sizeof(float);
+								break;
+							case "nx":
+								propertiesPositions.Add(Properties.NX, propertyIndex++);
+								vertexSize += sizeof(float);
+								break;
+							case "ny":
+								propertiesPositions.Add(Properties.NY, propertyIndex++);
+								vertexSize += sizeof(float);
+								break;
+							case "nz":
+								propertiesPositions.Add(Properties.NZ, propertyIndex++);
+								vertexSize += sizeof(float);
+								break;
+							case "s":
+								propertiesPositions.Add(Properties.S, propertyIndex++);
+								vertexSize += sizeof(float);
+								break;
+							case "t":
+								propertiesPositions.Add(Properties.T, propertyIndex++);
+								vertexSize += sizeof(float);
+								break;
+							default:
+								switch (tokens[1])
+								{
+									case "char":
+										vertexSize += 1;
+										break;
+									case "uchar":
+										vertexSize += 1;
+										break;
+									case "short":
+										vertexSize += 2;
+										break;
+									case "ushort":
+										vertexSize += 2;
+										break;
+									case "int":
+										vertexSize += 4;
+										break;
+									case "uint":
+										vertexSize += 4;
+										break;
+									case "float":
+										vertexSize += 4;
+										break;
+									case "double":
+										vertexSize += 8;
+										break;
+								}
+
+								propertiesPositions.Add(Properties.NONE, propertyIndex++);
+								break;
+						}
+
+						break;
+					case "end_header":
+						vertexListStart = file.Position;
+						faceListStart = vertexListStart + vertexAmount * vertexSize;
+						goto header_finished;
+				}
+			}
+
+		header_finished: ;
+		}
 	}
 
 	enum Format
